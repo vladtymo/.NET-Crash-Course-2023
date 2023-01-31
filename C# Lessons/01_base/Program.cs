1b088e8b37ab61f82590e998764a30898bb134e5
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2023-01-31 22:24:46 +0200

    Console. Data Types.

---
+++ C# Lessons/01_base/Program.cs
@@ -0,0 +1,43 @@
+﻿// inline comemnt
+/*
+    block comment
+*/
+
+// Ctrl+F5 - run applicaiton witout debugging
+
+// ------------- working with console
+Console.Title = "First C# Console Application";
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Hello C#!");
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Hello C#!");
+
+Console.WriteLine("First line");
+Console.WriteLine("Second line");
+Console.WriteLine("Third line");
+
+Console.Write("First word");
+Console.Write("Second word");
+Console.Write("Third word");
+Console.WriteLine();
+
+/* Escape sequences: 
+ *  \n - new line
+ *  \t - tabulation
+ *  \" - "
+ *  \' - '
+ *  \\ - \
+ */
+
+Console.WriteLine("Hello, my name is \"Vlad!\"");
+// snippet: cw + TAB
+Console.WriteLine("One\tTwo\tThree\n\\____________________\\");
+
+// ---------------- read data from console
+Console.Write("Enter login: ");
+string login = Console.ReadLine();
+Console.WriteLine($"Your login is {login}!");
+
+Console.ReadKey(); // press any key

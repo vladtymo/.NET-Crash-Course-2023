f63c2baa0fcbb9c7feca7bdb3242cb233c129847
Stepan-2007 <manua.stepan.2007@gmail.com>
2023-02-02 16:37:55 +0200

    task-2drop

--- task_2/task_enum/Program.cs
+++ task_2/task_enum/Program.cs
@@ -1,9 +1,37 @@
 ﻿
-enum week { Monday=1, Tuesday , Wednesday , Thursday, Friday, Saturday }
+enum Week { Monday=1, Tuesday , Wednesday , Thursday, Friday, Saturday, Sunday }
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Write("Enter number day: ");
+        Week day = Enum.Parse<Week>(Console.ReadLine());
+        switch (day)
+        {
+            case Week.Monday:
+                Console.WriteLine($"{Week.Monday}");
+                break;
+            case Week.Tuesday:
+                Console.WriteLine($"{Week.Tuesday}");
+                break;
+            case Week.Wednesday:
+                Console.WriteLine($"{Week.Wednesday}");
+                break;
+            case Week.Thursday:
+                Console.WriteLine($"{Week.Thursday}");
+                break;
+            case Week.Friday:
+                Console.WriteLine($"{Week.Friday}");
+                break;
+            case Week.Saturday:
+                Console.WriteLine($"{Week.Saturday}");
+                break;
+            case Week.Sunday:
+                Console.WriteLine($"{Week.Sunday}");
+                break;
+            default:
+                Console.WriteLine($"Invalid num day:{day}");
+                break;
+        }
     }
 }

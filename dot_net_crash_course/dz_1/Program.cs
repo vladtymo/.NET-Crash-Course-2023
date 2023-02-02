b507323765e50e9877a948e4539509ed7f573eba
MVozniak <mikola.v.2016@gmail.com>
2023-02-02 16:59:41 +0200

    Set ignore 1

---
+++ dot_net_crash_course/dz_1/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Zavd 1");
+        //CheckDate();
+        Console.WriteLine("Zavd 2");
+        //Rectangle();
+        Console.WriteLine("Zavd 3");
+        //Circle();
+        
+
+    }
+
+    //-----------------------------------------------------------------
+
+    public static void CheckDate()
+    {
+        float day = 0, month = 0, year = 0;
+
+        Console.WriteLine("Enter the current day: ");
+        day = CheckWordOnInt(day);
+
+        Console.WriteLine("Enter the current month: ");
+        month = CheckWordOnInt(month);
+
+        Console.WriteLine("Enter the current year: ");
+        year = CheckWordOnInt(year);
+
+        if ((day < 1 || day > 31) || (month < 1 || month > 12) || (year > 2023))
+        {
+            Console.WriteLine("Wrong input data!");
+        }
+        else
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Ccurrent date is: {day}/{month}/{year}");
+        }
+    }
+
+    //-----------------------------------------------------------------
+
+    public static int Rectangle()
+    {
+        float a = 0, b = 0;
+
+        a = CheckWordOnInt(a);
+        b = CheckWordOnInt(b);
+        
+        if (a < 0 || b < 0)
+        {
+            Console.WriteLine("Wrong input data!");
+        }
+        else
+        {
+            Console.WriteLine();
+            Console.WriteLine($"The area of rectangle is {a * b}");
+            Console.WriteLine($"The perimeter of rectangle is {(a + b)*2}");
+        }
+        return 0;
+    }
+
+
+    //-----------------------------------------------------------------
+
+    public static int Circle()
+    {
+        float r = 0;
+
+        r = CheckWordOnInt(r);
+
+        if (r < 0 )
+        {
+            Console.WriteLine("Wrong input data!");
+        }
+        else
+        {
+            Console.WriteLine();
+            Console.WriteLine($"The area of circle is {Math.PI*Math.Pow(r,2)}");
+        }
+        return 0;
+    }
+
+    //-----------------------------------------------------------------
+
+    private static float CheckWordOnInt(float word)
+    {
+        try
+        {
+            word = Convert.ToSingle(Console.ReadLine());
+            return word;
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Wrong input data!");
+        }
+
+        return 0;
+    }
+
+    //-----------------------------------------------------------------
+
+
+
+
+}
---
+++ dot_net_crash_course/enum_zavd/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq.Expressions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Zavd1();
+        Zavd2();
+
+
+    }
+
+    private static void Zavd1()
+    {
+        Console.WriteLine("Please enter the day (1,2,3,4,5,6,7): ");
+        int day = int.Parse(Console.ReadLine());
+        switch (day)
+        {
+            case 1:
+                Console.WriteLine("Monday");
+                break;
+            case 2:
+                Console.WriteLine("Tuesday");
+                break;
+            case 3:
+                Console.WriteLine("Wednesday");
+                break;
+            case 4:
+                Console.WriteLine("Thursday");
+                break;
+            case 5:
+                Console.WriteLine("Friday");
+                break;
+            case 6:
+                Console.WriteLine("Saturday");
+                break;
+            case 7:
+                Console.WriteLine("Sunday");
+                break;
+            default:
+                Console.WriteLine("Wrong day!!!");
+                break;
+        }
+    }
+
+    private static void Zavd2()
+    {
+
+    }
+
+
+}

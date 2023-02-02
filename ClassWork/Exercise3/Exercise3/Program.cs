9f1a23b38e737862124e835c2dfd958e36d01c5d
PetrushaT <petrusha_ak15@nuwm.edu.ua>
2023-02-02 22:12:55 +0200

    Завдання 3

---
+++ ClassWork/Exercise3/Exercise3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+enum Area { perimeter=1, area=2 , radius=3  };
+internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введiть оперерацiю яку потрiбно виконати\n" +
+"1)perimeter\n" +
+"2)area\n" +
+"3)radius");
+
+        Area CoolOperation = Enum.Parse<Area>(Console.ReadLine());
+        Console.WriteLine("Введiть дiаметр:");
+        double diameter = Convert.ToDouble(Console.ReadLine());
+        switch (CoolOperation) {
+            case Area.radius:
+                Console.WriteLine($"Радiус кола дорiвнює {diameter/2}");
+                break;
+            case Area.perimeter:
+                Console.WriteLine($"Периметр кола дорiвнює {2*Math.PI*(diameter / 2)}");
+                break;
+            case Area.area:
+                Console.WriteLine($"Площа кола дорiвнює {Math.PI*(diameter/2)*2}");
+                break;
+            default: Console.WriteLine("Введено не вiрне значення");
+                break;
+        }
+    }
+        
+
+}

e5c0589a4534a71263a7acbb1ae2cdd511129acb
Stepan-2007 <manua.stepan.2007@gmail.com>
2023-02-02 16:53:02 +0200

    task-2-ccurency

---
+++ task_2/task_exchange_currency/Program.cs
@@ -0,0 +1,26 @@
+﻿enum Ccurency { USD = 1, EUR, PLN }
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter sum money: ");
+        int sumMoney =int.Parse( Console.ReadLine());
+        Console.WriteLine("Choose ccurency:\n" +
+                            $"{(int)Ccurency.USD} - {Ccurency.USD}\n" +
+                            $"{(int)Ccurency.EUR} - {Ccurency.EUR}\n" +
+                            $"{(int)Ccurency.PLN} - {Ccurency.PLN}\n");
+
+        Ccurency exchange = Enum.Parse<Ccurency>(Console.ReadLine());
+        switch (exchange)
+        {
+            case Ccurency.USD:
+                break;
+            case Ccurency.EUR:
+                break;
+            case Ccurency.PLN:
+                break;
+            default:
+                break;
+        }
+    }
+}

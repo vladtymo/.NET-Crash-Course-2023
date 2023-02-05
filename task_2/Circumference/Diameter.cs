ccc790b1c58cea65fee130ffc70eca8c328ea3b5
Stepan-2007 <manua.stepan.2007@gmail.com>
2023-02-05 14:54:48 +0200

    task-diameter

---
+++ task_2/Circumference/Diameter.cs
@@ -0,0 +1,33 @@
+﻿enum Value { Radius = 1, Square, Perimeter }
+
+internal class Diameter
+{
+    private static void Main(string[] args)
+    {
+    
+        Console.Write("Enter diameter: ");
+        float diameter = float.Parse(Console.ReadLine());
+        Console.WriteLine("Choose function:\n" +
+                            $"{(int)Value.Radius} - Search {Value.Radius}\n" +
+                            $"{(int)Value.Square} - Search {Value.Square}\n" +
+                            $"{(int)Value.Perimeter} - Search {Value.Perimeter}\n");
+        Value function = Enum.Parse<Value>(Console.ReadLine());
+
+        switch (function)
+        {
+            case Value.Radius:
+                Console.WriteLine($"Radius = {diameter/2}");
+                break;
+            case Value.Square:
+                Console.WriteLine($"Square = {Math.PI* (diameter / 2)* (diameter / 2)}");
+                break;
+            case Value.Perimeter:
+                Console.WriteLine($"Perimeter = {2*Math.PI*(diameter / 2)}");
+                break;
+            default:
+                Console.WriteLine("Invalid value\n");
+                break;
+        }
+
+    }
+}

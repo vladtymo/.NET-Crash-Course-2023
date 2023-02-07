ab4975c09b49d8d97d955058d213aca34abd4066
bake1912 <102677146+bake1912@users.noreply.github.com>
2023-02-07 18:48:13 +0200

    Update Program.cs

--- C# Lessons/02_data_types/Program.cs
+++ C# Lessons/02_data_types/Program.cs
@@ -1,70 +1,77 @@
-﻿// --------------- data types
-Console.WriteLine("Data types...");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-/* simple types: 
- * - integers: short: 2, int: 4, long: 8
- * - floating-point: float: 4, dobule: 8, decimal: 16
- * - symbols: char, string
- * - logic: bool
- */
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+        {
+            /*
+            Console.WriteLine("Enter the current day");
+            string day = Console.ReadLine();
 
-Console.WriteLine("\tLiterals:");
-// interpolation: $"...{expression}..."
-Console.WriteLine($"Value: 123, type: {123.GetType()}");
-Console.WriteLine($"Value: 123L, type: {123L.GetType()}");
-Console.WriteLine($"Value: 55.6, type: {55.6.GetType()}");
-Console.WriteLine($"Value: 55.6F, type: {55.6F.GetType()}");
-Console.WriteLine($"Value: 55.6M, type: {55.6M.GetType()}");
-Console.WriteLine($"Value: 100000999999999999, type: {100000999999999999.GetType()}");
-Console.WriteLine($"Value: '#', type: {'#'.GetType()}");
-Console.WriteLine($"Value: \"Hello\", type: {"Hello".GetType()}");
-Console.WriteLine($"Value: true, type: {true.GetType()}");
 
-// create variable: type name = value;
-// variable naming: camelCaseConvension
-float width = 125.5F;
-float height = 220.5F;
-float area = width * height;
-string color = "Brown";
-string material = "Wood";
-bool isOpen = true;
+            Console.WriteLine("Enter the current mouth");
+            string moutn = Console.ReadLine();
 
-double a = 5 / (double)2;
-Console.WriteLine($"Result: {a}");
+            Console.WriteLine("Enter the current year");
+            string year = Console.ReadLine();
 
-Console.WriteLine($"Door info: {area / 10000}m^2 {color} made of {material} with status {isOpen}");
+            Console.WriteLine($"Today is {day}/{moutn}/{year}");
+            */
 
-// ----------- ariphmetic operations: + - * / %
-// ----------- modify value: += -= *= /= %=
+            /*
+            Console.WriteLine("Enter a side");
+            int a = int.Parse(Console.ReadLine());
 
-// ----------- type converting
-// string parse using type.Parse() method
-double doubleValue = double.Parse("55.2");
-int intValue = int.Parse("876");
+            Console.WriteLine("Enter a side");
+            int b = int.Parse(Console.ReadLine());
 
-Console.WriteLine($"Double value: {doubleValue}");
-Console.WriteLine($"Int value: {intValue}");
+            int resultSquare = a * b;
+            int resultPerimetr = a *2 +b*2;
 
-// type converting using Convert class
-decimal decimalValue = Convert.ToDecimal(doubleValue);
-Console.WriteLine($"Int value: {intValue}");
+            Console.WriteLine($"The square is {resultSquare} and the perimetr is {resultPerimetr}");
+            */
+            /*
+            Console.WriteLine("Enter the radius");
+            int radius = int.Parse(Console.ReadLine());
 
-// ----------- practical tasks -----------
-// task 4:
-Console.WriteLine("\n----------------- TASK 4 -----------------");
-Console.Write("Enter seconds: ");
-int seconds = int.Parse(Console.ReadLine()); // 8405 -> 2h 20m 5s
+            double resultSquare = Math.Pow(radius, 2)*Math.PI  ;
 
-const int secondsPerHour = 3600;
-const int secondsPerMinute = 60;
+            Console.WriteLine($"The radius is {Math.Floor(resultSquare)}");
+            */
 
-//secondsPerHour += 1; // cannot modify constants value
+            /*
+            Console.Write("Введіть рік: ");
+            int year = Int32.Parse(Console.ReadLine());
+            Console.Write("Кількість днів у році: ");
+            if (year % 4 == 0)
+            {
+                Console.WriteLine(365);
+            }
+            else
+            {
+                Console.WriteLine(364);
+            }
 
-int hours = seconds / secondsPerHour;
+            */
 
-seconds -= hours * secondsPerHour;              // 1205
-int minutes = seconds / secondsPerMinute;
 
-seconds -= minutes * secondsPerMinute;          // 5
 
-Console.WriteLine($"Time: {hours}:{minutes}:{seconds}");
+
+
+
+        }
+
+    }
+
+
+
+}
+
+    

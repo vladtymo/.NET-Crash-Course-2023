12540631a311f6a0e9bf8abdf7f05d7f13a1b3fb
Olena Sheremet <lenka.sheremet1978@gmail.com>
2023-02-08 16:19:24 +0200

    Add files via upload

---
+++ lab_2_enums.cs
@@ -0,0 +1,82 @@
+﻿enum Week { Monday = 1, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday };
+enum Currency { USD = 1, EUR = 2, PLN = 3 }
+enum Circle { Radius = 1, Area, Perimeter }
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Task 1");
+        Console.Write("Enter number of the day of week:" +
+            $"\n{(int)Week.Monday}-{Week.Monday}" +
+            $"\n{(int)Week.Tuesday}-{Week.Tuesday}" +
+            $"\n{(int)Week.Wednesday}-{Week.Wednesday}" +
+            $"\n{(int)Week.Thursday}-{Week.Thursday}" +
+            $"\n{(int)Week.Friday}-{Week.Friday}" +
+            $"\n{(int)Week.Saturday}-{Week.Saturday}" +
+            $"\n{(int)Week.Sunday}-{Week.Sunday}\n");
+        Week day = Enum.Parse<Week>(Console.ReadLine());
+        switch (day)
+        {
+            case Week.Monday: Console.WriteLine($"\n{Week.Monday}"); break;
+            case Week.Tuesday: Console.WriteLine($"\n{Week.Tuesday}"); break;
+            case Week.Wednesday: Console.WriteLine($"\n{Week.Wednesday}"); break;
+            case Week.Thursday: Console.WriteLine($"\n{Week.Thursday}"); break;
+            case Week.Friday: Console.WriteLine($"\n{Week.Friday}"); break;
+            case Week.Saturday: Console.WriteLine($"\n{Week.Saturday}"); break;
+            case Week.Sunday: Console.WriteLine($"\n{Week.Sunday}"); break;
+            default: Console.WriteLine("\nInvalid number!"); break;
+        }
+
+        Console.WriteLine("Task 2");
+        Console.Write("Enter amound of currency(UAN):");
+        int uan = int.Parse(Console.ReadLine());
+        const double usd = 36.75, eur = 40.12, pln = 8.56;
+        Console.Write("\nChoose which currency to convert to:" +
+            $"\n{(int)Currency.USD}-{Currency.USD}" +
+            $"\n{(int)Currency.EUR}-{Currency.EUR}" +
+            $"\n{(int)Currency.PLN}-{Currency.PLN}\n");
+        Currency currency = Enum.Parse<Currency>(Console.ReadLine());
+        switch (currency)
+        {
+            case Currency.USD: Console.WriteLine($"\n{uan} UAN = {uan / usd}"); break;
+            case Currency.EUR: Console.WriteLine($"\n{uan} UAN = {uan / eur} EUR"); break;
+            case Currency.PLN: Console.WriteLine($"\n{uan} UAN = {uan / pln} PLN"); break;
+            default: Console.WriteLine("\nInvalid currency!!!"); break;
+        }
+
+        Console.WriteLine("Task 3");
+        Console.Write("Enter diameter:");
+        double d = double.Parse(Console.ReadLine());
+        int k = 4;
+        while (k != 0)
+        {
+            Console.Write("\nChoose an action:" +
+                $"\n{(int)Circle.Radius}-{Circle.Radius}" +
+                $"\n{(int)Circle.Area}-{Circle.Area}" +
+                $"\n{(int)Circle.Perimeter}-{Circle.Perimeter}\n");
+            Circle action = Enum.Parse<Circle>(Console.ReadLine());
+            switch (action)
+            {
+                case Circle.Radius: Console.WriteLine($"\n{Circle.Radius} = {d / 2}"); break;
+                case Circle.Perimeter: Console.WriteLine($"\n{Circle.Perimeter} = {Math.PI * d}"); break;
+                case Circle.Area: Console.WriteLine($"\n{Circle.Area} = {(Math.PI * d * d) / 4}"); break;
+                default: Console.WriteLine("\nInvalid date!!!"); break;
+            }
+            --k;
+        }
+
+        Console.WriteLine("Task 4");
+        Console.WriteLine("Enter a number:");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int originalNumber = number;
+        int reverseNumber = 0;
+        while (number != 0)
+        {
+            int lastDigit = number % 10;
+            reverseNumber = reverseNumber * 10 + lastDigit;
+            number = number / 10;
+        }
+        if (originalNumber == reverseNumber) { Console.WriteLine($"{originalNumber} is palindrome"); }
+        else { Console.WriteLine($"{originalNumber} is not a palindrome"); }
+    }
+}
---
+++ lab_3_cycles.cs
@@ -0,0 +1,113 @@
+﻿internal class lab_3_cycles
+{
+    enum MenuArray { SumOfAllElements = 1, ArraySorting,NumberOfEven,MaximumValue };
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Task 1");
+        Console.WriteLine("Enter the length of the line:");
+        int lenght = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter a character fot the line:");
+        char character=Convert.ToChar(Console.ReadLine());
+        while(lenght!=0)
+        {
+            Console.Write($"{character}");
+            --lenght;
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Task 2");
+        Random rnd = new Random();
+        int randomNumber = rnd.Next(1,10);  //number from 1 to 10
+        int counter = 0;
+        int yourNumber=-1;
+        while(yourNumber!=randomNumber)
+        {
+            Console.WriteLine("Try to guess the number:");
+            yourNumber = Convert.ToInt32(Console.ReadLine());
+            ++counter;
+            if (yourNumber != randomNumber)
+            {
+                Console.WriteLine("You didn`t guess!");
+                if (yourNumber > randomNumber) Console.WriteLine($"{yourNumber} > riddled number!!!");
+                else if (yourNumber < randomNumber) Console.WriteLine($"{yourNumber} < riddled number!!!");
+            }
+        }
+        Console.WriteLine($"{counter} trainse were used");
+        Console.WriteLine();
+
+        Console.WriteLine("Task 3");
+        const int size = 20;
+        int[] array = new int[size];
+        for(int i=0;i<array.Length;++i)
+        {
+            int newRandomNmber = rnd.Next(0, 100);
+            array[i] = newRandomNmber;
+        }
+        foreach(int el in array)
+        {
+            Console.Write($"{el} ");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Task 4");
+        int k = 5;
+        while (k != 0)
+        {
+            --k;
+            Console.Write("\nChoose the action:" +
+               $"\n{(int)MenuArray.SumOfAllElements}-{MenuArray.SumOfAllElements}" +
+               $"\n{(int)MenuArray.ArraySorting}-{MenuArray.ArraySorting}" +
+               $"\n{(int)MenuArray.NumberOfEven} - {MenuArray.NumberOfEven}" +
+               $"\n{(int)MenuArray.MaximumValue}-{MenuArray.MaximumValue}\n");
+            MenuArray yourChoise = Enum.Parse<MenuArray>(Console.ReadLine());
+            Console.WriteLine();
+            switch (yourChoise)
+            {
+                case MenuArray.SumOfAllElements:
+                    {
+                        int sumOfALL = 0;
+                        for (int i = 0; i < array.Length; ++i)
+                        {
+                            sumOfALL += array[i];
+                        }
+                        Console.WriteLine($"Suma of all elements in array ={sumOfALL}");
+                    }
+                    break;
+                case MenuArray.ArraySorting:
+                    {
+                        Array.Sort(array);
+                        foreach (int el in array)
+                        {
+                            Console.Write($"{el} ");
+                        }
+                        Console.WriteLine();
+                    }
+                    break;
+                case MenuArray.NumberOfEven:
+                    {
+                        int count = 0;
+                        for (int i = 0; i < array.Length; ++i)
+                        {
+                            if (array[i] % 2 == 0) ++count;
+                        }
+                        Console.WriteLine($"The number of even ={count}");
+                    }
+                    break;
+                case MenuArray.MaximumValue:
+                    {
+                        int maxValue = array[0];
+                        for (int i = 1; i < array.Length; ++i)
+                        {
+                            if (maxValue < array[i]) maxValue = array[i];
+                        }
+                        Console.WriteLine($"Maximum value = {maxValue}, in position {Array.IndexOf(array, maxValue)}.");
+                    }
+                    break;
+                default: Console.WriteLine("\nInvalid value!"); break;
+            }
+
+        }
+    }
+
+}

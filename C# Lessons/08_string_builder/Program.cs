abb5a05ec5b3e333e7ce10f283833622e2d8c9f2
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2023-02-09 19:34:57 +0200

    String class example. Methods. StringBuilder.

---
+++ C# Lessons/08_string_builder/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+// bad
+string str = "My name is Vlad!";
+str += "\n";
+str += "The last line!";
+
+// good
+StringBuilder builder = new StringBuilder();
+
+Console.WriteLine("Length: " + builder.Length);
+Console.WriteLine("Capacity: " + builder.Capacity);
+
+builder.Append("My name is Vlad!");
+builder.AppendLine();
+builder.AppendLine("The last line!");
+
+Console.WriteLine("Length: " + builder.Length);
+Console.WriteLine("Capacity: " + builder.Capacity);
+
+Console.WriteLine("Reuslt:\n" + builder.ToString());

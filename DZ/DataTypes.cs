9982f69a7e0a051183d2a4cd97d3d1e5548aa4d4
bake1912 <102677146+bake1912@users.noreply.github.com>
2023-02-09 16:52:22 +0200

    Add files via upload

---
+++ DZ/DataTypes.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            /*
+            Console.WriteLine("Enter the current day");
+            string day = Console.ReadLine();
+            Console.WriteLine("Enter the current mouth");
+            string moutn = Console.ReadLine();
+            Console.WriteLine("Enter the current year");
+            string year = Console.ReadLine();
+            Console.WriteLine($"Today is {day}/{moutn}/{year}");
+            */
+
+            /*
+            Console.WriteLine("Enter a side");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a side");
+            int b = int.Parse(Console.ReadLine());
+            int resultSquare = a * b;
+            int resultPerimetr = a *2 +b*2;
+            Console.WriteLine($"The square is {resultSquare} and the perimetr is {resultPerimetr}");
+            */
+            /*
+            Console.WriteLine("Enter the radius");
+            int radius = int.Parse(Console.ReadLine());
+            double resultSquare = Math.Pow(radius, 2)*Math.PI  ;
+            Console.WriteLine($"The radius is {Math.Floor(resultSquare)}");
+            */
+
+            /*
+            Console.Write("Введіть рік: ");
+            int year = Int32.Parse(Console.ReadLine());
+            Console.Write("Кількість днів у році: ");
+            if (year % 4 == 0)
+            {
+                Console.WriteLine(365);
+            }
+            else
+            {
+                Console.WriteLine(364);
+            }
+            */
+
+
+
+
+
+
+        }
+
+    }
+
+
+
+}

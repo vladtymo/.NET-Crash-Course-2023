0babc2b145ad90b8dc691998a57aa5aecd19a01b
Kok1ysh <yalovenko.lybomur@gmail.com>
2023-02-10 19:49:43 +0200

    Create Task 2.4.cs

---
+++ Hometasks/Task2/Task 2.4.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть число");
+            string x = Console.ReadLine();
+
+            char[] xx = x.ToCharArray();
+            Array.Reverse(xx);
+            string xxx = new string(xx);
+
+            if (x == xxx)
+            {
+                Console.WriteLine("Число поліндром");
+            }
+            else
+            {
+                Console.WriteLine("Число не поліндром");
+            }
+
+        }
+    }
+}

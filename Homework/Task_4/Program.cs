d4948cd24f8fff03f3d75e3ec61dcf0b84f7dbdb
Oleg <olegredko45@gmail.com>
2023-02-13 16:41:52 +0200

    update Homework Tasks#4

--- Homework/Task_4/Program.cs
+++ Homework/Task_4/Program.cs
@@ -5,26 +5,50 @@ namespace Tasks_4
 {
     class Program
     {
-        public static void Main(string[] args)
+		/*
+         * Завдання:
+
+         1. Вводиться рядок слів. Вивести слова в зворотньому порядку.
+         
+         2. Обрахувати кількість пробілів в рядку, яку введе користувач.
+         
+         3. Дано текст. Визначте відсоткове відношення малих і великих літер до загальної кількості символів в ньому.
+         
+         4. Написати функцію, яка приймає словосполучення і перетворює його в абревіатуру. 
+         Наприклад: cascading style sheets в CSS, об'єктно орієнтоване програмування в ООП.
+         
+         5. Користувач вводить слова, поки не буде введено слово з символом крапки вкінці.
+         Сформувати з введених слів рядок, розділивши їх комою з пробілом.
+         
+         *6. У введеному рядку видалити пробіли між першим і другим символом крапки.
+         
+         */
+
+		public static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-            // Console.WriteLine(Task1_Version_1("Hello world!"));
-            // Task2("Hello world !");
+            //Task1_Version_1("Hello world!");// !dlrow olleH
+			//Task1_Version_2("Hello world!");// world! Hello
 
-            // Task3("Abcderfsat");
+			
+            //Console.WriteLine($"Кількість пробілів в рядку = {Task2("Hello world !")}");
+			
+            //Task3("AAbb");
 
-            // Console.WriteLine(Task4("об'єктно орієнтоване програмування"));
+			// Console.WriteLine(Task4("об'єктно орієнтоване програмування"));
 
 
-            // Console.WriteLine(Task5());
-            Task6();
+			// Console.WriteLine(Task5());
+			
+           // Task6();
           
             Console.ReadKey();
         }
-        static string Task1_Version_1(string input)
-        {
-            char[] charArray = input.ToCharArray();
+		#region  1. Вводиться рядок слів. Вивести слова в зворотньому порядку.
+		static void Task1_Version_1(string line)
+		{
+            char[] charArray = line.ToCharArray();
             
 
             StringBuilder stringBuilder= new StringBuilder();
@@ -33,20 +57,23 @@ namespace Tasks_4
                 stringBuilder.Append(charArray[i]);
                
             }
-            return stringBuilder.ToString();
+            Console.WriteLine(stringBuilder.ToString());
         }
         
         static void Task1_Version_2(string line)
         {
-            string[] word =  line.Split(' ');
-            Array.Reverse(word);
-            foreach (string word2 in word)
+            string[] words =  line.Split(' ');
+            Array.Reverse(words);
+            foreach (string word in words)
             {
-                Console.Write(word2 + " ");
+                Console.Write(word + " ");
             }
             Console.WriteLine();
         }
-        static void Task2(string line)
+		#endregion
+
+		#region  2. Обрахувати кількість пробілів в рядку, яку введе користувач.
+		static int Task2(string line)
         {
             int counter = 0;
             for(int i = 0; i < line.Length; i++)
@@ -54,116 +81,113 @@ namespace Tasks_4
             {
                     ++counter;
             }
-            Console.WriteLine($"Кількість пробілів в рядку = {counter}");
+            return counter;
+           
         }
-        static void Task3(string line)
+		#endregion
+
+		#region  3. Дано текст. Визначте відсоткове відношення малих і великих літер до загальної кількості символів в ньому.
+		static void Task3(string line)
         {
-            int counterUpper = 0,counter = 0 ;
+           
+            int countUpper = 0,countLower = 0 ;
             for (int i = 0; i < line.Length; i++)
                 if (line[i] >= 'A' && line[i] <= 'Z')
                 {
-                    ++counterUpper;
+                    ++countUpper;
                 }
-                else ++counter;
+                else ++countLower;
 
            
-            Console.WriteLine($"Відсоток великих літер =  {(counterUpper / (double)line.Length)*100}%");
-            Console.WriteLine($"Відсоток малих літер =  {(counter / (double)line.Length) * 100}%");
+            Console.WriteLine($"Відсоток великих літер =  {(countUpper / (double)line.Length)*100}%");
+            Console.WriteLine($"Відсоток малих літер =  {(countLower / (double)line.Length) * 100}%");
            
         }
-        /*
-         * 4. Написати функцію, яка приймає словосполучення і перетворює його в абревіатуру. 
-            Наприклад: cascading style sheets в CSS, об'єктно орієнтоване програмування в ООП.
+		#endregion
 
+		#region 4. Написати функцію, яка приймає словосполучення і перетворює його в абревіатуру. 
+		//Наприклад: cascading style sheets в CSS, об'єктно орієнтоване програмування в ООП.
 
-
-*6. У введеному рядку видалити пробіли між першим і другим символом крапки.
-         */
-        static string Task4(string line)
+		static string Task4(string line)
         {
-            string[] lines= line.Split(" ");
-            StringBuilder stringBuilder= new StringBuilder();
-            for(int i=0; i< lines.Length;i++)
+            string[] words = line.Split(" ");
+            StringBuilder result = new StringBuilder();
+            for(int i=0; i< words.Length;i++)
             {
-                stringBuilder.Append(lines[i].Remove(1));
+                result.Append(words[i].Remove(1));
                 //string newWord = lines[i].Remove(1);
                 
             }
             
            
-            return stringBuilder.ToString().ToUpper();
-        }
-        //5. Користувач вводить слова, поки не буде введено слово з символом крапки вкінці.
-       // Сформувати з введених слів рядок, розділивши їх комою з пробілом.
-        static string Task5()
+            return result.ToString().ToUpper();
+		}
+		#endregion
+
+		#region  5. Користувач вводить слова, поки не буде введено слово з символом крапки вкінці. Сформувати з введених слів рядок, розділивши їх комою з пробілом.
+
+		static string Task5()
         {
-            string input = "";
+            string line = "";
            
             StringBuilder result = new StringBuilder();
-            while (input != ".")
+
+            while (line != ".")
             {
                 Console.Write("Enter a word: ");
-                input = Console.ReadLine();
-                if (!string.IsNullOrWhiteSpace(input))
+                line = Console.ReadLine();
+
+                if (!string.IsNullOrWhiteSpace(line))
                 {
-                    if (input != ".") result.Append(input + " , ");
+                    if (line != ".") 
+                        result.Append(line + " , ");
                 }
                 else
-                    Console.WriteLine("The field must not be empty");
+                    Console.WriteLine("The line must not be empty");
                 
                 
             }
 
             return ("Result: " + result.ToString().TrimEnd(',', ' '));
-           
-            //// str.Replace(' ', '-')
-            // StringBuilder stringBuilder = new StringBuilder();
-            // string[] lines = (Console.ReadLine());
-
-
-            // while (true)
-            // {
-            //     stringBuilder.Append(Console.ReadLine());
-            //     if (stringBuilder.ToString().Contains('.'))
-            //     {
-
-            //         break;
-            //     }
-
-            // }
-            // string a = stringBuilder.ToString().Join(" / ", stringBuilder.ToString());
-            // Console.WriteLine(stringBuilder.ToString());
-
-
         }
-        //*6. У введеному рядку видалити пробіли між першим і другим символом крапки.
+		#endregion
 
-        public static void Task6()
+		#region 6. У введеному рядку видалити пробіли між першим і другим символом крапки.
+		public static void Task6()
         {
-            string input = "This is a first string. Second string is also here. Third string is here. Fourth string is there";
-            //input = Console.ReadLine();
-            StringBuilder result = new StringBuilder();
-        
-            int startIndex = input.IndexOf(".");
-
-            if (startIndex != -1) 
-            { 
-                string res = input.Substring(0, startIndex + 1);
-                result.Append(res);
-            Console.WriteLine(res + " 1");
-            }
+            string input = "This is a first string. Second string is also here. Third string is here. Fourth string is there.";
            
-            input = input.Substring(startIndex + 1);
-            Console.WriteLine(input + " 2");
-            startIndex = input.IndexOf(".");
-            if (startIndex != -1)
-            {
-                string res = input.Substring(0, startIndex);
-                res = res.Replace(" ", "");
-                Console.WriteLine(res + " 3");
-                result.Append(res);
+            StringBuilder result = new StringBuilder();//наш результат
+        
+            int dotIndex = input.IndexOf(".");// Знаходимо індекс крапки
+
+            if (dotIndex != -1) 
+            { 
+                string temp = input.Substring(0, dotIndex + 1);//Обрізаємо перше речення
+                result.Append(temp);// додаємо в наш результат
+               
+           
+           
+                input = input.Substring(dotIndex + 1); // Тепер будемо працювати з іншою частиною речення
+                
+                dotIndex = input.IndexOf(".");//Знаходимо другу крапку
+
+                if(dotIndex != -1)
+                {
+
+                    temp = input.Substring(0, dotIndex);// Обрізаємо речення так щоб в нас вийшло речення між двох крапок
+                    temp = temp.Replace(" ", "");// Забираємо в ньому пробіли
+                                                 
+                    result.Append(temp);// додаємо в наш результат
+
+
+                    result.Append(input.Substring(dotIndex)); // додаємо ту частину речення яка залишилася 
+                    Console.WriteLine("Result: " + result.ToString());
+                }
+                else Console.WriteLine("This line does not have a second dot.");
             }
-            result.Append(input.Substring(startIndex)); Console.WriteLine(" string builder : " + result.ToString());
+            else Console.WriteLine("This line does not have a dot.");
         }
-    }
+		#endregion
+	}
 }

b3a56762a97fa92944c1964cc958aea653db341a
PetrushaT <petrusha_ak15@nuwm.edu.ua>
2023-02-14 15:00:42 +0200

    4-5 завдання добавлено

---
+++ HomeWork/Home3/Home3.4/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine('Введіть строку для відображення абривіатури');
+string InputText=Console.ReadLine();
+string[] words = InputText.Split(new[] { ' ', '.', '?', ',' }, StringSplitOptions.RemoveEmptyEntries);
+foreach (string word in words) 
+{
+    Console.Write(word.ToUpper()[0]);
+}
---
+++ HomeWork/Home3/Home3.5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.Tracing;
+using System.Text;
+
+StringBuilder builder = new StringBuilder();
+Console.WriteLine("Введіть слова");
+do
+{
+    string word = Console.ReadLine();
+    builder.Append(word+", ");
+
+    Console.WriteLine("Replaced: " + str.Replace(' ', '-'));
+    if (word.EndsWith("."))
+    {
+
+        break;
+    };
+
+} while (1==1);
+
+Console.WriteLine("Reuslt:\n" + builder.ToString());

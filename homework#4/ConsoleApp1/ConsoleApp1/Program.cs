edc779758ea65f69318b87d666fcb9ca49f99f73
oleksandrtymoshchuk1 <124050132+oleksandrtymoshchuk1@users.noreply.github.com>
2023-02-15 21:44:15 +0200

    Add files via upload

---
+++ homework#4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿///////////task1//////////////
+Console.WriteLine("Enter string: ");
+string str = Console.ReadLine();
+string[] slova = str.Split(' ', ',', '.', '?', '!');
+for (int i = slova.Length-1; i >= 0; i= i-1)
+{
+    Console.Write(slova[i] + " ");
+}
+
+///////////task2//////////////
+
+int count;
+Console.Write("Enter string: ");
+string str1 = Console.ReadLine();
+count = str1.Split(' ').Length;
+Console.WriteLine(count-1);
+
+///////////task3//////////////
+///

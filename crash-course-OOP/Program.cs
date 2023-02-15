2f0477fa326fee58a44d1b1ef68857c52067ee21
SaskoUsername <108696911+SaskoUsername@users.noreply.github.com>
2023-02-15 21:19:18 +0200

    Add files via upload

---
+++ crash-course-OOP/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace crash_course_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon glock = new Weapon();
+            Weapon RPG = new Weapon();
+            glock.Initialize("Glock-17", 50, 9, 17, 17);
+            RPG.Initialize("RPG-7", 700, 40, 1, 1);
+
+            Console.WriteLine(glock.Shot() ? "Boom!" : "Magazine is empty!");
+            Console.WriteLine(RPG.Shot() ? "Big boom!" : "Magazine is empty!");
+
+            using (StreamWriter guns_info = new StreamWriter("guns.txt"))
+            {
+                RPG.Save(guns_info);
+                glock.Save(guns_info);
+                guns_info.Close();  
+            }
+
+            Load();
+        }
+
+        static void Load()
+        {
+            using (StreamReader info = new StreamReader("guns.txt"))
+            {
+                Console.WriteLine(info.ReadToEnd());
+            }
+        }
+    }
+}

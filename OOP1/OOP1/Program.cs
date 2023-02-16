2f16b87122886a831af9882f35f823e8918dc945
CKAZO4NICK <andriipriyma@gmail.com>
2023-02-16 12:28:35 +0200

    OOP1

---
+++ OOP1/OOP1/Program.cs
@@ -0,0 +1,62 @@
+﻿class Weapon
+{
+    int range;
+    double caliber;
+    int MagazineSize;
+    int CurentBulletsInMagazine;
+
+   public void Initialize(int r, double c, int Mag)
+    {
+        range = r;
+        caliber = c;
+        MagazineSize = Mag;
+    }
+    public void Show()
+    {
+        Console.WriteLine($"Range: {range}\n"
+           + $"Caleber: {caliber}\n"
+           + $"Magazine Size: {MagazineSize}\n"
+           + $"Current Bullets In Magazine: {CurentBulletsInMagazine}");
+    }
+    public void Reload()
+    {
+        Console.WriteLine("Weapon was reloaded!");
+        CurentBulletsInMagazine = MagazineSize;
+    }
+    public void Shot()
+    {
+        if (CurentBulletsInMagazine != 0)
+        {
+            CurentBulletsInMagazine--;
+            Console.WriteLine($"Weapon Shooted! Bullets left: {CurentBulletsInMagazine}");
+        }
+        else
+            Console.WriteLine("Not enough bullets in magazine! Reload the weapon!");
+    }
+}
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+            Weapon show = new Weapon();
+            show.Initialize(1000, 7.62, 30);
+            show.Show();
+        do
+        {
+            Console.WriteLine("Що бажаєте зробити?\n"+"1 - reload\n"+"2 - shoot\n"+"3 - check info\n");
+            string s = Console.ReadLine();
+            switch (s)
+                {
+                case "1": show.Reload();
+                    break;
+                case "2": show.Shot();
+                    break;
+                case"3": show.Show();
+                    break;
+                default: Console.WriteLine("Wrong number!");
+                    break;
+            }
+        } while (Console.ReadLine() != "Exit" || Console.ReadLine() != "exit");
+    }
+}

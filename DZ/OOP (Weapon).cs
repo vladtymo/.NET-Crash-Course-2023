1c150d50ea253925a1d049aec2975c0a64ec00b7
bake1912 <102677146+bake1912@users.noreply.github.com>
2023-02-16 15:56:05 +0200

    Add files via upload

---
+++ DZ/OOP (Weapon).cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+
+namespace Code_Coach_Challenge
+{
+
+    class Weapon
+    {
+        int range;
+        float caliber;
+        int maxSize;
+        public int amount;
+
+
+        public void initialize(int range,float caliber,int maxSize)
+        {
+            this.range = range;
+            this.caliber = caliber;
+            this.maxSize = maxSize;
+            amount = maxSize;
+            Console.WriteLine($"The max distance: {range} caliber: {caliber}" +
+                $" max size {maxSize}  ");
+
+
+        }
+        public bool shot()
+        {
+           -- amount;
+            return amount == 0 ? false : true;
+        }
+        public void reCharge()
+        {
+            amount = maxSize;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon weapon = new Weapon();
+            weapon.initialize(300,5.56f,30);
+            weapon.shot();
+            weapon.shot();
+            weapon.shot();
+            weapon.reCharge();
+
+
+            Console.WriteLine(weapon.amount);
+            
+
+
+
+
+        }
+
+
+
+
+    }
+}
+
+

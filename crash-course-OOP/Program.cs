43c6f2d5f191cb7bb4814f249537807ad677184b
SaskoUsername <108696911+SaskoUsername@users.noreply.github.com>
2023-02-17 17:18:24 +0200

    Add files via upload

---
+++ crash-course-OOP/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace crash_course_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon glock = new Weapon();
+            Weapon RPG = new Weapon();
+            glock.Initialize("Glock-17", 50, 9, 17, 17);
+            RPG.Initialize("RPG-7", 700, 40, 1, 1);
+
+            Console.WriteLine(glock.Shot() ? "Boom!" : "Magazine is empty!");
+            Console.WriteLine(RPG.Shot() ? "Big boom!" : "Magazine is empty!");
+
+            glock.Save("glock.json");
+            RPG.Save("rpg.json");
+
+            Weapon.Load("rpg.json");
+            
+        }
+
+    }
+}
---
+++ crash-course-OOP/Weapon.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace crash_course_OOP
+{
+    internal class Weapon
+    {
+        [JsonProperty]
+        private string name;
+        
+        [JsonProperty]
+        private int range;
+        
+        [JsonProperty]
+        private int caliber;
+        
+        [JsonProperty]
+        private int curr_bullets;
+
+        [JsonProperty]
+        private int max_bullets;
+
+        public void Initialize(string name, int range, int caliber, int curr_bullets, int max_bullets)
+        {
+            this.name = name;   
+            this.range = range;
+            this.caliber = caliber;
+            this.curr_bullets= curr_bullets;
+            this.max_bullets = max_bullets;
+        }
+
+        public bool Shot()
+        {
+            if (curr_bullets > 0)
+            {
+                --curr_bullets;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+
+        public void Recharge()
+        {
+            curr_bullets = max_bullets;
+            Console.WriteLine("Gun was recharged");
+        }
+
+        
+        public void Save(string filePath)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                string output = JsonConvert.SerializeObject(this, Formatting.Indented);
+                writer.Write(output);
+            }
+        }
+
+        public static Weapon Load(string filePath)
+        {
+            string weaponInfo;
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                weaponInfo = reader.ReadToEnd();
+            }
+
+            Console.Write(weaponInfo);
+            Weapon weapon = JsonConvert.DeserializeObject<Weapon>(weaponInfo);
+
+            return weapon;
+        }
+    }
+}

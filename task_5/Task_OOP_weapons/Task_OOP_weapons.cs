a52fd6657fa2f4ec9fd881bf0cac9e944ddb466b
Stepan-2007 <manua.stepan.2007@gmail.com>
2023-02-18 11:07:05 +0200

    task_classes and show task_4

---
+++ task_5/Task_OOP_weapons/Task_OOP_weapons.cs
@@ -0,0 +1,67 @@
+﻿
+class Weapon
+{
+    int range;
+    float caliber;
+    int patronInMagaz;
+    int maxSizeMagaz;
+    public void Initialize(int range, float caliber, int maxSizeMagaz)
+    {
+        this.range = range;
+        this.caliber = caliber;
+        this.maxSizeMagaz = maxSizeMagaz;
+    }
+    public bool Shot()
+    {
+        if (this.patronInMagaz == 0) { Console.WriteLine("Shot did not happen, reload the magazine"); return false; }
+        else { Console.WriteLine("Shot"); this.patronInMagaz -= 1; return true; }
+    }
+    public void Recharge()
+    {
+        Console.WriteLine($"Before reloading: {this.patronInMagaz}");
+        if(this.patronInMagaz < this.maxSizeMagaz)this.patronInMagaz=this.maxSizeMagaz;else Console.WriteLine("MaxSize Magazin");
+        Console.WriteLine($"After reloading: {this.patronInMagaz}");
+    }
+    public async void Save()
+    {
+        string[] setup =
+        {
+            this.range.ToString(),this.caliber.ToString(),this.patronInMagaz.ToString() ,this.maxSizeMagaz.ToString()+"\n"
+        };
+        await File.WriteAllLinesAsync("write.txt", setup);
+    }
+    public void Load()
+    {
+        string[] setup = System.IO.File.ReadAllLines(@".\write.txt");
+        this.range = int.Parse(setup[0]);
+        this.caliber = float.Parse(setup[1]);
+        this.patronInMagaz = int.Parse(setup[2]);
+        this.maxSizeMagaz = int.Parse(setup[3]);
+    }
+    public void Print()
+    {
+        Console.WriteLine(this.range.ToString() + " m \n"
+        +this.caliber.ToString() + " mm \n"
+        +this.patronInMagaz.ToString() + " ammo\n"
+        +this.maxSizeMagaz.ToString() + " ammo\n");
+    }
+}
+internal class Task_OOP_weapons
+{
+    private static void Main(string[] args)
+    {
+     Weapon pistol=new Weapon();
+        pistol.Initialize(100, 9.0F, 15);
+        pistol.Print();
+        pistol.Recharge();
+        pistol.Shot();
+        pistol.Save();
+        pistol.Print();
+        Weapon copygun = new Weapon();
+        copygun.Load();
+        copygun.Print();
+        copygun.Recharge();
+        copygun.Shot();
+        copygun.Print();
+    }
+}
---
+++ task_5/Task_class_ctor/Task_class_ctor.cs
@@ -0,0 +1,118 @@
+﻿
+using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
+
+class Plane
+{
+    private readonly string name;
+    private readonly string brand;
+    private const int maxHeight=11000;//м
+    private int heightNow=0;
+    private const int maxSpeed=850;//км\год
+    private int speedNow=0;
+    public string getName { get { return this.name; } }
+    public string getBrand { get { return this.brand; } }
+    public int getHeightNow { get { return this.heightNow; } }
+    public int getSpeedNow { get { return this.speedNow; } }
+    static bool IsValidHeight(int heightNow)
+    {
+       if(heightNow < maxHeight && heightNow >= 0)return true;
+       return false;
+    }
+    static bool IsValidSpeed(int speedNow)
+    {
+        if (speedNow < maxSpeed && speedNow >= 0)return true;
+        return false;
+    }
+    public Plane()
+    {
+        this.name = "Noname";
+        this.brand = "Nobrend";
+        this.heightNow = 0;
+        this.speedNow = 0;
+    }
+    public Plane(string name,string brand)
+    {
+        this.name = name;
+        this.brand = brand;
+        this.heightNow = 0;
+        this.speedNow = 0;
+    }
+    public Plane(string name, string brand, int heightNow, int speedNow)
+    {
+        this.name = name;
+        this.brand = brand;
+        if(IsValidHeight(heightNow))this.heightNow = heightNow;
+        if(IsValidSpeed(speedNow))this.speedNow = speedNow;
+    }
+    public void Print()
+    {
+        Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+        Console.WriteLine($"Name: {this.name}");
+        Console.WriteLine($"Brand: {this.brand}");
+        Console.WriteLine($"HeightNow: {this.heightNow} m");
+        Console.WriteLine($"SpeedNow: {this.speedNow} km\\h");
+        Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+    }
+    public void ShowLimites()
+    {
+        Console.WriteLine("********************************");
+        Console.WriteLine($"Limit height {maxHeight} m");
+        Console.WriteLine($"Limit speed {maxSpeed} km\\h");
+        Console.WriteLine("********************************");
+    }
+    public void Setup(int heightNow,int speedNow)
+    {
+        if (IsValidHeight(heightNow)&&IsValidSpeed(speedNow))
+        {
+            this.heightNow = heightNow;
+            this.speedNow = speedNow;
+            Console.WriteLine($"Height to {heightNow} and speed to {speedNow} changed");
+        } else Console.WriteLine("Height and speed not changed, invalid value");
+        Console.WriteLine("----------------------------------");
+    }
+    public void SetHeight(int heightNow)
+    {
+        if (IsValidHeight(heightNow))
+        {
+            this.heightNow = heightNow;
+            Console.WriteLine($"Height changed to {heightNow}");
+        }else Console.WriteLine("Height not changed, invalid value");
+        Console.WriteLine("----------------------------------");
+    }
+    public void SetSpeed(int speedNow)
+    {
+        if (IsValidSpeed(speedNow))
+        {
+            this.speedNow = speedNow;
+            Console.WriteLine($"Speed changed to {speedNow}");
+        } else Console.WriteLine("Speed not changed, invalid value");
+        Console.WriteLine("----------------------------------");
+    }
+
+}
+
+namespace Task_class_ctor
+{
+    internal class Task_class_ctor
+    {
+        static void Main(string[] args)
+        {
+         Plane plane = new Plane();
+            plane.Print();
+            Plane plane1 = new Plane("Mriya","An-225",9000,400);
+            plane1.Print();
+            plane1.SetHeight(1000);
+            plane1.SetSpeed(300);
+            Console.WriteLine($"Height: {plane1.getHeightNow}");
+            Console.WriteLine($"Speed: {plane1.getSpeedNow}");
+            plane1.Print();
+            Plane plane2 = new Plane("Ruslan", "An-124");
+            Console.WriteLine($"Name: {plane2.getName}\nBrand: {plane2}");
+            plane2.Print();
+            plane2.Setup(4000, 450);
+            plane2.Print();
+
+        }
+    }
+}

b4846f20b1d742f82f016add92e9cc5fc235a413
Swayze <kovaloff10@gmail.com>
2023-02-21 00:20:09 +0200

    Swayze task 6

---
+++ Hometasks/ConsoleApp6/ConsoleApp6/Car.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    public class Car
+    {
+        private readonly string car_name;
+        private int car_year;
+        private int? car_speed = null;
+        private const int car_maxprice = 5000000;
+        private bool? car_fixed = null;
+        private int base_speed = 120;
+
+
+        public Car(string car_name, int car_year, int car_speed, bool? car_fixed)
+        {
+            this.car_name = car_name;
+            this.car_year = car_year;
+            this.car_speed = car_speed;
+            this.car_fixed = car_fixed;
+        }
+        public Car(string car_name, int car_year, int car_speed)
+        {
+            this.car_name = car_name;
+            this.car_year = car_year;
+            this.car_speed = car_speed;
+        }
+        public Car(string car_name, int car_year)
+        {
+            this.car_name = car_name;
+            this.car_year = car_year;
+        }
+
+        public void Car_info()
+        {
+            Console.WriteLine($"Car named: {car_name}.\n" +
+             $"Car year: {car_year}\n" +
+             $"Car speed: {(car_speed != null ? car_speed.ToString() : base_speed.ToString())}\n" +
+             $"Car is repaired?: {(car_fixed != null ? car_fixed.ToString() : "Unknown")}");
+        }
+
+        public void UpSpeed(int speed)
+        {
+            if (car_speed == null)
+            {
+                base_speed += speed;
+            }
+            if (speed > 0)
+            {
+                car_speed += speed;
+            }
+            else
+            {
+                Console.WriteLine("I cant up speed to negative value");
+            }
+        }
+
+        public void ChangeYear(int car_year)
+        {
+            this.car_year = car_year;
+        }
+
+        public bool isFixed(bool fix)
+        {
+            if (fix == true){
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Hometasks/ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            Car[] cars = new Car[3];
+            cars[0] = new Car("Ford Shelby GT 500", 1969);
+            cars[1] = new Car("Koenigsegg Agera RS", 2012, 415);
+            cars[2] = new Car("Ford GT40", 1970, 240, false);
+
+            foreach (Car car in cars)
+            {
+                car.Car_info();
+                car.UpSpeed(random.Next(1,100));
+                car.ChangeYear(random.Next(1998, 2023));
+                Console.WriteLine("Changed\n\n");
+                car.Car_info();
+                Console.WriteLine();
+            }
+        }
+    }
+}

1d23f47f1f00a9c370c7223a20c2fd4615fbe02d
Sorias05 <sashko2b3@gmail.com>
2023-02-21 11:34:30 +0200

    Homework 6

---
+++ Hometasks/HW6/HW6/Computer.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static HW6.RAM;
+
+namespace HW6
+{
+    internal class Laptop
+    {
+        private readonly string name;
+        private int memory { get; set; }
+        private int ram { get; set; }
+        private CPU cpu { get; set; }
+        private GPU gpu { get; set; }
+
+        public Laptop() { }
+        public Laptop(string name, CPU cpu, GPU gpu, int ram, int memory) 
+        {
+            this.name = name;
+            this.cpu = cpu;
+            this.gpu = gpu;
+            this.ram = ram;
+            this.memory = memory;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Laptop:\nName: {name};");
+            cpu.Print();
+            gpu.Print();
+            Console.WriteLine($"RAM: {ram} GB");
+            Console.WriteLine($"Memory: {memory} GB\n");
+        }
+
+        public void AddMemory(int memory)
+        {
+            this.memory += memory;
+        }
+        public void AddRam(int ram)
+        {
+            this.ram += ram;
+        }
+    }
+
+    internal class PC
+    {
+        private MotherBoard motherBoard { get; set; }
+        private List<Drive> drives { get; set; }
+        private List<RAM> rams { get; set; }
+        private CPU cpu { get; set; }
+        private GPU gpu { get; set; }
+        private PowerSupply powerSupply { get; set; }
+
+        public PC() { }
+        public PC(MotherBoard motherBoard, CPU cpu, GPU gpu, PowerSupply powerSupply, List<RAM> rams, List<Drive> drives) 
+        {
+            this.motherBoard = motherBoard;
+            this.cpu = cpu;
+            this.gpu = gpu;
+            this.powerSupply = powerSupply;
+            this.rams = rams;
+            this.drives = drives;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("PC:");
+            cpu.Print();
+            gpu.Print();
+            Console.WriteLine("RAM:");
+            PrintRam();
+            Console.WriteLine("Memory:");
+            PrintDrives();
+            powerSupply.Print();
+            Console.WriteLine();
+        }
+        
+        private void PrintRam()
+        {
+            foreach(RAM ram in rams)
+                ram.Print();
+        }
+
+        private void PrintDrives()
+        {
+            foreach (Drive drive in drives)
+                drive.Print();
+        }
+
+        public void AddRam(RAM ram)
+        {
+            rams.Add(ram);
+        }
+
+        public void AddDrive(Drive drive)
+        {
+            drives.Add(drive);
+        }
+    }
+
+    internal class CPU
+    { 
+        private string name;
+        private int frequency;
+        private int cores;
+        public CPU(string name, int frequency, int cores)
+        {
+            this.name = name;
+            this.frequency = frequency;
+            this.cores = cores;
+        }
+        public void Print() { Console.WriteLine($"CPU: Name: {name},\tCores: {cores},\tFrequency: {frequency} MHz;"); }
+    }
+
+    internal class GPU
+    {
+        private string name;
+        private int videoMemory;
+        public GPU(string name, int videoMemory)
+        {
+            this.name = name;
+            this.videoMemory = videoMemory;
+        }
+        public void Print() { Console.WriteLine($"GPU: Name: {name},\tVideo Memory: {videoMemory} GB;"); }
+    }
+
+    internal class RAM
+    {
+        public enum RamType { SDR, DDR, DDR2, DDR3, DDR4, DDR5 }
+        private string name;
+        private int memory;
+        private int frequency;
+        private RamType ramType;
+        public RAM(string name, int memory, int frequency, RamType ramType)
+        {
+            this.name = name;
+            this.memory = memory;
+            this.frequency = frequency;
+            this.ramType = ramType;
+        }
+        public void Print() { Console.WriteLine($"Name: {name},\tRAM Type: {ramType.ToString()},\tMemory: {memory} GB,\tFrequency: {frequency} MHz;"); }
+    }
+
+    internal class Drive
+    {
+        public enum DriveType { HDD, SSD, SSD_M2, Flash }
+        private string name;
+        private int memory;
+        private int frequency;
+        private DriveType driveType;
+        public Drive(string name, int memory, int frequency, DriveType driveType)
+        {
+            this.name = name;
+            this.memory = memory;
+            this.frequency = frequency;
+            this.driveType = driveType;
+        }
+        public void Print() { Console.WriteLine($"Name: {name},\tDrive Type: {driveType.ToString()},\tMemory: {memory} GB,\tFrequency: {frequency} MHz;"); }
+    }
+
+    internal class MotherBoard
+    {
+        public enum FormFactor { Pico_ITX, Nano_ITX, Mini_ITX, Micro_ATX, ATX, E_ATX }
+        private string name;
+        private string socket;
+        private FormFactor formFactor;
+        public MotherBoard(string name, string socket, FormFactor formFactor)
+        {
+            this.name = name;
+            this.socket = socket;
+            this.formFactor = formFactor;
+        }
+        public void Print() { Console.WriteLine($"MotherBoard: Name: {name},\tForm Factor: {formFactor.ToString()},\tSocket: {socket};"); }
+    }
+
+    internal class PowerSupply 
+    {
+        private string name;
+        private int power;
+        public PowerSupply(string name, int power)
+        {
+            this.name = name;
+            this.power = power;
+        }
+        public void Print() { Console.WriteLine($"Power Supply: Name: {name},\tPower: {power} watt;"); }
+    }
+}
---
+++ Hometasks/HW6/HW6/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace HW6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Laptop laptop = new Laptop
+                ( 
+                "Lenovo Legion 5", 
+                new CPU("AMD Ryzen 7 4800H", 12, 3200), 
+                new GPU("NVIDIA GeForce RTX 2060", 4),
+                16, 
+                2048
+                );
+
+            PC pc = new PC
+                (
+                new MotherBoard("MSI MAG B550 TOMAHAWK", "AM4", MotherBoard.FormFactor.Micro_ATX),
+                new CPU("AMD Ryzen 5 5600X", 12, 3200),
+                new GPU("NVIDIA GeForce RTX 2060", 4),
+                new PowerSupply("Gamemax GM-500B", 500),
+                new List<RAM> { new RAM("Kingston Fury Beast", 8, 3200, RAM.RamType.DDR4), new RAM("Kingston Fury Beast", 8, 3200, RAM.RamType.DDR4) },
+                new List<Drive> { new Drive("SSD GOODRAM CL100 GEN 3 SSDPR-CL100-960-G3", 960, 3200, Drive.DriveType.SSD), new Drive("Seagate BarraCuda Compute ST2000DM008", 2048, 7200, Drive.DriveType.HDD) }
+                );
+
+            laptop.Print();
+            pc.Print();
+
+            laptop.AddRam(16);
+            laptop.AddMemory(512);
+            Console.WriteLine("Laptop: Added ram and memory...\n");
+            laptop.Print();
+
+            pc.AddRam(new RAM("Kingston Fury Beast", 8, 3200, RAM.RamType.DDR4));
+            pc.AddDrive(new Drive("Kingston FURY Renegade", 4096, 3200, Drive.DriveType.SSD_M2));
+            Console.WriteLine("PC: Added ram and memory...\n");
+            pc.Print();
+        }
+    }
+}

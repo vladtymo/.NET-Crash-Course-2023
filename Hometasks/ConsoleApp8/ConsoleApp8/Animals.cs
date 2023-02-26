2aef2b30145912b9fef16951bda87bfc67060382
Swayze <kovaloff10@gmail.com>
2023-02-26 23:28:30 +0200

    Swayze task 8

---
+++ Hometasks/ConsoleApp8/ConsoleApp8/Animals.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    public class Animal
+    {
+        string name;
+        public string names { 
+            get
+            {
+                return name;
+            } 
+        }
+        int speed;
+        string type;
+        int weight;
+        string living;
+        string sound;
+
+        public Animal(string name, int speed, string type, int weight, string living, string sound)
+        {
+            this.name = name;
+            this.speed = speed;
+            this.type = type;
+            this.weight = weight;
+            this.living = living;
+            this.sound = sound;
+        }
+
+        public void Move()
+        {
+            Random rnd = new Random();
+
+            Console.WriteLine($"{type}'s has moved to {rnd.Next(1,10)} metrs");
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine(sound);
+        }
+        public void Show_info()
+        {
+            Console.WriteLine($"Name of {type}: {name}\n" + 
+                $"Speed of {type}: {speed}\n" +
+                $"Weight of {type}: {weight}\n" +
+                $"{type} living enviroment: {living}\n" +
+                $"{name} have sound: {sound}");
+        }
+
+    }
+
+    public class Bird : Animal
+    {
+        string name;
+        public string names
+        {
+            get
+            {
+                return name;
+            }
+        }
+        int speed;
+        string type;
+        int weight;
+        string living;
+        string sound;
+        public Bird(string name, int speed, string type, int weight, string living, string sound) : base(name, speed, type, weight, living , sound)
+        {
+            this.name = name;
+            this.speed = speed;
+            this.type = type;
+            this.weight = weight;
+            this.living = living;
+            this.sound = sound;
+        }
+    }
+    public class Fish : Animal
+    {
+        string name;
+        public string names
+        {
+            get
+            {
+                return name;
+            }
+        }
+        int speed;
+        string type;
+        int weight;
+        string living;
+        string sound;
+        public Fish(string name, int speed, string type, int weight, string living, string sound) : base(name, speed, type, weight, living, sound)
+        {
+            this.name = name;
+            this.speed = speed;
+            this.type = type;
+            this.weight = weight;
+            this.living = living;
+            this.sound = sound;
+        }
+
+        public new void Move()
+        {
+            Console.WriteLine($"{type} is swimming");
+        }
+
+        public new void MakeSound()
+        {
+            Console.WriteLine($"{type} is not making any sound");
+        }
+    }
+    public class Reptile : Animal
+    {
+        string name;
+        public string names
+        {
+            get
+            {
+                return name;
+            }
+        }
+        int speed;
+        string type;
+        int weight;
+        string living;
+        string sound;
+        public Reptile(string name, int speed, string type, int weight, string living, string sound) : base(name, speed, type, weight, living, sound)
+        {
+            this.name = name;
+            this.speed = speed;
+            this.type = type;
+            this.weight = weight;
+            this.living = living;
+            this.sound = sound;
+        }
+    }
+}
---
+++ Hometasks/ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {          
+            Animal animal = new Animal("Harvey", 50, "animal", 890, "Forest", "Grrrr");
+            animal.Show_info();
+            animal.MakeSound();
+            animal.Move();
+
+            Bird bird = new Bird("Kesha", 20, "bird", 1, "Air", "Nothing");
+            
+            Fish fish = new Fish("Aqua", 2, "fish", 3, "Water", "Nothing");
+
+            Reptile reptile = new Reptile("Hameleon", 15, "Reptile", 1 ,"Anything", "Nothing");
+
+            List<string> zoo= new List<string>();
+
+            zoo.Add(animal.names);
+            zoo.Add(bird.names);
+            zoo.Add(fish.names);
+            zoo.Add(reptile.names);
+
+            foreach(string s in zoo)
+            {
+                Console.WriteLine(s);
+            }
+            
+        }
+    }
+}

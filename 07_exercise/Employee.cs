6c5b8632892c548b6f8cdce57775a4adeb5f4e3a
Anastasia <95645924+hmaaraaa@users.noreply.github.com>
2023-02-26 18:11:52 +0200

    Add files via upload

---
+++ 07_exercise/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterpriseStructure
+{
+
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+        public decimal Salary { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Surname: {Surname}, Birth Date: {BirthDate}, Salary: {Salary}";
+        }
+    }
+}

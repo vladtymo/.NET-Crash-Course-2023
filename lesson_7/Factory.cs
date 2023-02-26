47962acf7354247f3bb2e382d1acb435d3e3adde
Karina Zubko <karinazubko2017@gmail.com>
2023-02-26 17:41:16 +0200

    homework for the seventh lesson

---
+++ lesson_7/Factory.cs
@@ -0,0 +1,71 @@
+using System;
+
+namespace EnterpriseStructure
+{
+    class Factory
+    {
+        public string Name { get; set; }
+        public Employee[] Employees { get; set; }
+        public Product[] Products { get; set; }
+
+        public decimal AvgSalary
+        {
+            get
+            {
+                decimal totalSalary = 0;
+                foreach (Employee employee in Employees)
+                {
+                    totalSalary += employee.Salary;
+                }
+                return totalSalary / Employees.Length;
+            }
+        }
+
+        public decimal TotalSalary
+        {
+            get
+            {
+                decimal totalSalary = 0;
+                foreach (Employee employee in Employees)
+                {
+                    totalSalary += employee.Salary;
+                }
+                return totalSalary;
+            }
+        }
+
+        public decimal GDP
+        {
+            get
+            {
+                decimal totalCost = 0;
+                foreach (Product product in Products)
+                {
+                    totalCost += product.Price;
+                }
+                return totalCost / Employees.Length;
+            }
+        }
+
+        public int EmpCount
+        {
+            get
+            {
+                return Employees.Length;
+            }
+        }
+
+        public Factory(string name, Employee[] employees, Product[] products)
+        {
+            Name = name;
+            Employees = employees;
+            Products = products;
+        }
+
+        public override string ToString()
+        {
+            return $"Factory: {Name}";
+        }
+    }
+}
+

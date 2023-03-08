90dc161f3ab9ded576cbe7b5f751264c9d9fb053
Olena Sheremet <lenka.sheremet1978@gmail.com>
2023-03-08 18:37:15 +0200

    Interfaces

--- Homework 9/Task1/DVD.cs
+++ Homework 9/Task1/DVD.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace lab_09_task1
 {
-    internal class DVD :
+    internal class DVD : Disk, IRemoveableDisk
     {
+        private bool hasDisk;
+        public bool HasDisk { get; set; }
+        public void GetName()
+        {
+            Console.WriteLine("DVD Name");
+        }
+
+        public void Insert()
+        {
+            Console.WriteLine("DVD method Insert");
+        }
+
+        public void Reject()
+        {
+            Console.WriteLine("DVD method Reject");
+        }
+
     }
 }

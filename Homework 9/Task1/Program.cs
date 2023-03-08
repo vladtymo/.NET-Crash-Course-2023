e073fee239e59e9e4029183b6b0dd93af4327f24
Olena Sheremet <lenka.sheremet1978@gmail.com>
2023-03-08 18:50:21 +0200

    Interfaces

--- Homework 9/Task1/Program.cs
+++ Homework 9/Task1/Program.cs
@@ -2,9 +2,44 @@
 {
     internal class Program
     {
+        static void Read(IDisk item)
+        {
+            item.Read();
+        }
+        static void Write(IDisk item)
+        {
+            item.Write();
+        }
+        static void Insert(IRemoveableDisk item)
+        {
+            item.Insert();
+        }
+        static void Reject(IRemoveableDisk item)
+        {
+            item.Reject();
+        }
+        static void GetName(IPrintInformation item)
+        {
+            item.GetName();
+        }
+        static void Print(string str)
+        { Console.WriteLine(str); }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Disk newDisk = new Disk("DDR3", 4);
+
+            Write(neweDisk);
+
+            DVD newDiskDVD = new DVD();
+            Insert(newDiskDVD);
+
+            Printer newPrinter= new Printer();
+            Print("I can print text");
+
+            Comp computer = new Comp(1,2);
+            computer.WriteInfo("This is a string", "Printer");
+
         }
     }
 }

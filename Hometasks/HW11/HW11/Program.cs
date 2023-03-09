24e7cfff3fe894ffde60e44ec28991a4aabbe65b
Sorias05 <sashko2b3@gmail.com>
2023-03-09 18:56:56 +0200

    Homework 11

---
+++ Hometasks/HW11/HW11/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace HW11
+{
+    internal class Program
+    {
+        internal struct SPoint
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public SPoint(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            SPoint[] points = { new SPoint(6, 6), new SPoint(20, 20), new SPoint(2, 5) };
+            Line line = new Line(new SPoint(5, 3), new SPoint(15, 13));
+            Rectangle rectangle = new Rectangle(new SPoint(5, 14), 10, 10);
+            Polyline polyline = new Polyline(points);
+            line.Print();
+            rectangle.Print();
+            polyline.Print();
+        }
+    }
+}
---
+++ Hometasks/HW11/HW11/Shape.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static HW11.Program;
+
+namespace HW11
+{
+    enum ShapeType { Line, Rectangle, Polyline }
+    internal class Shape
+    {
+        public ShapeType Type { get; set; }
+
+        public virtual void Print()
+        {
+            Console.WriteLine("---");
+        }
+    }
+
+    internal class Line : Shape
+    {
+        public SPoint A { get; set; }
+        public SPoint B { get; set; }
+
+        public Line(SPoint a, SPoint b)
+        {
+            A = a;
+            B = b;
+        }
+        public override void Print()
+        {
+            double length = Math.Sqrt(Math.Pow(B.X - A.X, 2) + Math.Pow(B.Y - A.Y, 2));
+            for (int i = 0; i <= length; i++)
+            {
+                int x = A.X + (int)(i * (B.X - A.X) / length);
+                int y = A.Y + (int)(i * (B.Y - A.Y) / length);
+                Console.SetCursorPosition(x, y);
+                Console.Write("*");
+            }
+        }
+    }
+
+    internal class Rectangle : Shape
+    {
+        public SPoint StartPoint { get; set; }
+        public double Width { get; set; }  
+        public double Height { get; set; }
+
+        public Rectangle(SPoint startPoint, double width, double height)
+        {
+            StartPoint = startPoint;
+            Width = width;
+            Height = height;
+        }
+        public override void Print()
+        {
+            Console.SetCursorPosition(StartPoint.X, StartPoint.Y);
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+
+    internal class Polyline : Shape
+    {
+        public SPoint[] Points { get; set; }
+
+        public Polyline(SPoint[] points)
+        {
+            Points = points;
+        }
+        public override void Print()
+        {
+            foreach(SPoint point in Points)
+            {
+                Console.SetCursorPosition(point.X, point.Y);
+                Console.WriteLine("*");
+            }
+        }
+    }
+}

ceae131672d12645185d55b37bf36f0b0e8e4770
Olena Sheremet <lenka.sheremet1978@gmail.com>
2023-03-09 14:53:06 +0200

    System Intefaces

--- Homework 10/Movie.cs
+++ Homework 10/Movie.cs
@@ -6,7 +6,47 @@ using System.Threading.Tasks;
 
 namespace lab_10_system_interfaces
 {
-    internal class Movie
+    public enum Ganre{Comedy,Horror,Adventure,Drama,Historical,Fantasy}
+
+    internal class Movie : ICloneable, IComparable<Movie>
     {
+       public string Title {get; set;}
+       public Director Director {get; set;}
+       public string Country{get; set;}
+       public Genre Genre{get; set;}
+       public int Year{get; set;}
+       public short Rating{get; set;}
+       
+       public Movie(string Title, Director Director, string Country, Ganre Ganre,int Year,short Rating)
+       { 
+           this.Title=Title;
+           this.Director=Director;
+           this.Country=Country;
+           this.Ganre=Ganre;
+           this.Year = Year;
+           this.Rating = Rating;
+       }
+       public override string ToString()
+       {
+           return $"Title: {Title}, Genre: {Genre}, Year: {Year}, Rating: {Rating}.";
+       }
+       public object Clone()
+       {
+           return new Movie(Title, (Director)Director.Clone(), Country, Genre, Year, Rating);
+       }
+       public int CompareTo(Movie otherMovie)
+        {
+            return Title.CompareTo(otherMovie.Title);
+        }
+       public int CompareToYear(Movie otherMovie)
+       {
+           if(other_movie == null){ return 1;}
+           return Year.CompareTo(otherMovie.Year);
+       }
+       public int CompareToRating(Movie otherMovie)
+       {
+           if(otherMovie == null){ return 1;}
+           return Rating.CompareTo(otherMovie.Rating);
+       }
     }
 }

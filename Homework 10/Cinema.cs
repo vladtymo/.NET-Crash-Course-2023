cef7a39067bf35b6fe922a16d496e6318905faa9
Olena Sheremet <lenka.sheremet1978@gmail.com>
2023-03-09 15:13:21 +0200

    Update Cinema.cs

--- Homework 10/Cinema.cs
+++ Homework 10/Cinema.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace lab_10_system_interfaces
 {
-    internal class Cinema
+    internal class Cinema : IEnumerable<Movie>
     {
+        private List<Movie> movies = new List<Movie>();
+        public string Address { get; set; }
+        
+        public override string ToString()
+        {
+           return $"The Address of the cinema:{Address} Number of movies: {movies.Count} ";
+        }
+        public void AddMovie(Movie movie)
+        {
+           movies.Add(movie);
+        }
+        public void Sort()
+        {  
+           movies.Sort();
+        }
+        public void Sort(IComparer<Movie> comparer)
+        {
+           movies.Sort(comparer);
+        }
+        public IEnumerator<Movie> GetEnumerator()
+        {
+           return movies.GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+           return GetEnumerator();
+        }
     }
 }

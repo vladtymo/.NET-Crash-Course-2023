5c7d46fba54bb9063ca348801f620c70321b7ac4
Olena Sheremet <lenka.sheremet1978@gmail.com>
2023-03-09 15:45:19 +0200

    System Interfaces

--- Homework 10/Program.cs
+++ Homework 10/Program.cs
@@ -2,9 +2,35 @@
 {
     internal class Program
     {
+        public class MovieCompareToYear: IComparer<Movie>
+        {
+            public int Compare(Movie x, Movie y)
+            {
+                return  x.Year.CompareTo(y.Year);
+            }
+        }
+        public class MovieCompareToRating: IComparer<Movie>
+        {
+            public int CompareToYear(Movie x, Movie y)
+            {
+                return  x.Rating.CompareTo(y.Rating);
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Movie movie1=new Movie("Harry Potter and the Prisoner of Azkaban",new Director("Alfonso","Cuarón"),"Ukraine",Ganre.Fantasy,2004,8);
+            Movie copyMovie1=(Movie)movie1.Clone();
+            Console.WriteLine($"Original: {movie1}");
+            Console.WriteLine($"Clone: {copyMovie1}";
+            copyMovie1.Title="Harry Potter 2"
+            copyMovie.Year=2000;
+            Cinema cinema=new Cinema();
+            cinema.AddMovie(movie1);
+            cinema.AddMovie(copyMovie1);
+            //cinema.Sort(new MovieCompareToYear());
+            
+            
+            
         }
     }
 }

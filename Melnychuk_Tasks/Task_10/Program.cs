1d8c41a3b30dcdbbeb5eabf44afea323462829bb
Sviatoslav <svyatoslav200104@gmail.com>
2023-03-11 11:00:48 +0200

    Add 11.

--- Melnychuk_Tasks/Task_10/Program.cs
+++ Melnychuk_Tasks/Task_10/Program.cs
@@ -93,5 +93,23 @@ public class Task
 
     public static void Main(string[] args)
     {
+
+        Director director1 = new Director("FirstName1", "LastName1");
+        Movie movie1 = new Movie("title1", director1, "country1", 0, 2334, 5);
+        Director director2 = new Director("FirstName2", "LastName2");
+        Movie movie2 = new Movie("title2", director2, "country2", 1, 2133, 9);
+        Director director3 = new Director("FirstName3", "LastName3");
+        Movie movie3 = new Movie("title3", director3, "country3", 2, 1932, 7);
+        Director director4 = new Director("FirstName4", "LastName4");
+        Movie movie4 = new Movie("title4", director4, "country4", 3, 1456, 5);
+
+        Movie[] movies = { movie1, movie2, movie3, movie4 };
+        Cinema cinema = new Cinema(movies, "Address");
+
+       cinema.Sort();
+        foreach (Movie item in cinema)
+        {
+            Console.WriteLine(item.ToString());
+        }
     }
 }
---
+++ Melnychuk_Tasks/Task_11/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+
+public class Task
+{
+    class Shape
+    {
+        public char Symbol { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public virtual void Print() { }
+
+    }
+
+    class Line : Shape
+    {
+        public Line(int x, int y, char symbol, int symbols)
+        {
+            X = x;
+            Y = y;
+            Symbol = symbol;
+            Symbols = symbols;
+        }
+        public char Symbol { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Symbols { get; set; }
+        public override void Print()
+        {
+            Console.SetCursorPosition(this.X, this.Y);
+
+            for (int y = 0; y < Symbols; y++)
+            {
+                Console.Write(Symbol);
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    class Rectangle : Shape
+    {
+
+        public Rectangle(int x, int y, char symbol, int height, int width)
+        {
+            X = x;
+            Y = y;
+            Symbol = symbol;
+            Height = height;
+            Width = width;
+        }
+        public char Symbol { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public override void Print()
+        {
+
+            for (int y = 0; y < Height; y++)
+            {
+                Console.SetCursorPosition(this.X, this.Y + y);
+
+                for (int x = 0; x < Width; x++)
+                {
+                    Console.Write(Symbol);
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+
+
+    class Polyline : Shape
+    {
+        public Polyline(int x, int y, char symbol)
+        {
+            X = x;
+            Y = y;
+            Symbol = symbol;
+        }
+        public char Symbol { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    public static void Main(string[] args)
+    {
+
+        Line line = new Line(3, 4, 'b', 5);
+        line.Print();
+
+        Rectangle rectangle = new Rectangle(4, 5, 'v', 5, 10);
+        rectangle.Print();
+
+        Console.ReadLine();
+    }
+}

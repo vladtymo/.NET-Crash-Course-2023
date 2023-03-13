778019743acfc21e2accbdf847fd768194f50341
Olena Sheremet <lenka.sheremet1978@gmail.com>
2023-03-13 15:06:02 +0200

    System Interfaces

--- Homework 10/Movie.cs
+++ Homework 10/Movie.cs
@@ -4,50 +4,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace lab_10_system_interfaces
 {
-    public enum Ganre{Comedy,Horror,Adventure,Drama,Historical,Fantasy}
+    public enum Ganre { Comedy, Horror, Adventure, Drama, Historical, Fantasy }
 
     internal class Movie : ICloneable, IComparable<Movie>
     {
-       public string Title {get; set;}
-       public Director Director {get; set;}
-       public string Country{get; set;}
-       public Ganre Ganre{get; set;}
-       public int Year{get; set;}
-       public short Rating{get; set;}
-       
-       public Movie(string Title, Director Director, string Country, Ganre Ganre,int Year,short Rating)
-       { 
-           this.Title=Title;
-           this.Director=Director;
-           this.Country=Country;
-           this.Ganre=Ganre;
-           this.Year = Year;
-           this.Rating = Rating;
-       }
-       public override string ToString()
-       {
-           return $"Title: {Title}, Genre: {Ganre}, Year: {Year}, Rating: {Rating}.";
-       }
-       public object Clone()
-       {
-           return new Movie(Title, (Director)Director.Clone(), Country, Ganre, Year, Rating);
-       }
-       public int CompareToTitle(Movie otherMovie)
+        public string Title { get; set; }
+        public Director Director { get; set; }
+        public string Country { get; set; }
+        public Ganre Ganre { get; set; }
+        public int Year { get; set; }
+        public short Rating { get; set; }
+
+        public Movie(string Title, Director Director, string Country, Ganre Ganre, int Year, short Rating)
+        {
+            this.Title = Title;
+            this.Director = Director;
+            this.Country = Country;
+            this.Ganre = Ganre;
+            this.Year = Year;
+            this.Rating = Rating;
+        }
+        public override string ToString()
+        {
+            return $"Title: {Title}, Genre: {Ganre}, Year: {Year}, Rating: {Rating}.";
+        }
+        public object Clone()
+        {
+            return new Movie(Title, (Director)Director.Clone(), Country, Ganre, Year, Rating);
+        }
+        public int CompareToTitle(Movie otherMovie)
         {
             return Title.CompareTo(otherMovie.Title);
         }
-       public int CompareToYear(Movie otherMovie)
-       {
-           if(otherMovie == null){ return 1;}
-           return Year.CompareTo(otherMovie.Year);
-       }
-       public int CompareToRating(Movie otherMovie)
-       {
-           if(otherMovie == null){ return 1;}
-           return Rating.CompareTo(otherMovie.Rating);
-       }
+        public int CompareToYear(Movie otherMovie)
+        {
+            if (otherMovie == null) { return 1; }
+            return Year.CompareTo(otherMovie.Year);
+        }
+        public int CompareToRating(Movie otherMovie)
+        {
+            if (otherMovie == null) { return 1; }
+            return Rating.CompareTo(otherMovie.Rating);
+        }
+
+        public int CompareTo(Movie? other)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

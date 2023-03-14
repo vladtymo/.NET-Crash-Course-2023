17d69daa63e36cb98d0dd7b5f521f16a7e47ff77
Sorias05 <sashko2b3@gmail.com>
2023-03-14 20:17:55 +0200

    Homework 12

---
+++ Hometasks/HW12/HW12/CombatVehicle.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW12
+{
+    enum CombatVehicleType { Tank, ArmoredCar, AirDefenseVehicle }
+    internal abstract class CombatVehicle
+    {
+        public CombatVehicleType Type { get; set; }
+        public string? Model { get; set; }
+        public double Health { get; set; }
+
+        public CombatVehicle(CombatVehicleType type, double health)
+        {
+            this.Type = type;
+            this.Health = health;
+        }
+        public CombatVehicle(CombatVehicleType type, string model, double health)
+        {
+            this.Type = type;
+            this.Model = model;
+            this.Health = health;
+        }
+        
+        public bool isDestroyed()
+        {
+            return Health <= 0;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Type: {Type}\nModel: {Model}\nHealth: {Health}\n");
+        }
+        public abstract double Attack();
+        public abstract void Defense(double damage);
+    }
+
+    internal class Tank : CombatVehicle
+    {
+        public double R { get; set; }
+        public double A { get; set; }
+        public double T { get; set; }
+        
+        public Tank(string model, double health, double r, double a, double t)            :base(CombatVehicleType.Tank, model, health)        {
+            R = r;
+            A = a;
+            T = t;
+        }
+        public Tank(double health, double r, double a, double t)            : base(CombatVehicleType.Tank, health)        {
+            R = r;
+            A = a;
+            T = t;
+        }
+
+        public override double Attack()
+        {
+            return (100 * A / R);
+        }
+        public override void Defense(double damage)
+        {
+            Health -= (damage - T);
+        }
+    }
+
+    internal class ArmoredCar : CombatVehicle
+    {
+        public int C { get; set; }
+        public double S { get; set; }
+
+        public ArmoredCar(string model, double health, int c, double s)            : base(CombatVehicleType.ArmoredCar, model, health)        {
+            C = c;
+            S = s;
+        }
+        public ArmoredCar(double health, int c, double s)            : base(CombatVehicleType.ArmoredCar, health)        {
+            C = c;
+            S = s;
+        }
+
+        public override double Attack()
+        {
+            return (50 * C);
+        }
+        public override void Defense(double damage)
+        {
+            Health -= (damage - S / 2);
+        }
+    }
+
+    internal class AirDefenseVehicle : CombatVehicle
+    {
+        public double L { get; set; }
+        public double R { get; set; }
+        public double M { get; set; }
+
+        public AirDefenseVehicle(string model, double health, double l, double r, double m)            : base(CombatVehicleType.AirDefenseVehicle, model, health)        {
+            L = l;
+            R = r;
+            M = m;
+        }
+        public AirDefenseVehicle(double health, double l, double r, double m)            : base(CombatVehicleType.AirDefenseVehicle, health)        {
+            L = l;
+            R = r;
+            M = m;
+        }
+
+        public override double Attack()
+        {
+            return (150 + L * (R / 10));
+        }
+        public override void Defense(double damage)
+        {
+            Health -= (damage / M);
+        }
+    }
+}
---
+++ Hometasks/HW12/HW12/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace HW12
+{
+    internal class Program
+    {
+        bool Round(CombatVehicle cv1, CombatVehicle cv2)
+        {
+            while(!cv1.isDestroyed() && !cv2.isDestroyed())
+                cv2.Defense(cv1.Attack()); cv1.Defense(cv2.Attack());
+            if(cv1.isDestroyed())
+                return false;
+            else 
+                return true;
+        }
+
+        void Battle(List<CombatVehicle> army1, List<CombatVehicle> army2)
+        {
+            Console.WriteLine("------------ Battle started! ------------");
+            int r = 0;
+            while (army1.Count > 0 && army2.Count > 0) 
+            {
+                int l;
+                if (army1.Count < army2.Count) l = army1.Count;
+                else l = army2.Count;
+                for(int i = 0; i < l; i++) 
+                {
+                    r++;
+                    Console.WriteLine($"Round {r} started!");
+                    if (Round(army1[i], army2[i]))
+                    {
+                        army2.RemoveAt(i);
+                        Console.WriteLine($"Army 1 got a winner in round {r}!");
+                    }
+                    else
+                    {
+                        army1.RemoveAt(i);
+                        Console.WriteLine($"Army 2 got a winner in round {r}!");
+                    }
+                    l--;
+                }
+            }
+            if(army1.Count > army2.Count)
+                Console.WriteLine("Army 1 won!");
+            else if(army1.Count < army2.Count)
+                Console.WriteLine("Army 2 won!");
+            else
+                Console.WriteLine("Draw!");
+        }
+
+        static void Main(string[] args)
+        {
+            Program P = new Program();
+            List<CombatVehicle> army1 = new()
+            {
+                new Tank("Tiger II", 1800, 7, 0.8, 5),
+                new ArmoredCar(2000, 8, 100),
+                new AirDefenseVehicle(1200, 1000, 6, 5),
+                new Tank(2000, 8, 0.6, 7),
+                new ArmoredCar(1750, 10, 120),
+                new AirDefenseVehicle(1400, 900, 7, 3)
+            };
+            List<CombatVehicle> army2 = new()
+            {
+                new Tank(2000, 8, 0.6, 7),
+                new ArmoredCar(1750, 10, 120),
+                new AirDefenseVehicle(1400, 900, 7, 3),
+                new Tank("Tiger II", 1800, 7, 0.8, 5),
+                new ArmoredCar(2000, 8, 100),
+                new AirDefenseVehicle(1200, 1000, 6, 5)
+            };
+            P.Battle(army1, army2);
+        }
+    }
+}

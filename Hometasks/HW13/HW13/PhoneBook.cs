80220eab47ae4f8492dd2408e7f0331e5f934e9d
Sorias05 <sashko2b3@gmail.com>
2023-03-16 20:01:12 +0200

    Homework 13

---
+++ Hometasks/HW13/HW13/PhoneBook.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW13
+{
+    internal class PhoneBook
+    {
+        public Dictionary<string, string> Book { get; set; }
+
+        public PhoneBook(Dictionary<string, string> book)
+        {
+            Book = book;
+        }
+        public void Add(string name, string phone)
+        {
+            Book.Add(name, phone);
+        }
+        public void Update(string item, string newKey, string newValue)
+        {
+            if (Book.ContainsKey(item) || Book.ContainsValue(item))
+                Book.Remove(item);
+            Book.Add(newKey, newValue);
+        }
+        public void Search(string item)
+        {
+            foreach (var el in Book)
+            {
+                if (item == el.Key || item == el.Value)
+                    Console.WriteLine($"{el.Key}:\t{el.Value}");
+                else
+                    Console.WriteLine("Not found...");
+            }
+        }
+        public void Remove(string item)
+        {
+            Book.Remove(item);
+        }
+    }
+}
---
+++ Hometasks/HW13/HW13/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace HW13
+{
+    internal class Program
+    {
+        static void Ex1()
+        {
+            List<string> words = new() { "trip", "deer", "similar", "collapse", "stunning", "recruit", "spit", "fraud", "tail", "jungle" };
+            List<string> results = new List<string>();
+            string result = "";
+            foreach (string word in words)
+            {
+                Console.Write(word + " ");
+                if (word.Length > result.Length)
+                    result = word;
+            }
+            foreach (string word in words)
+            {
+                if (word.Length >= result.Length)
+                    results.Add(word);
+            }
+            results.Sort();
+            Console.WriteLine();
+            Console.WriteLine(results[0]);
+        }
+        static void Main(string[] args)
+        {
+            Ex1();
+
+        }
+    }
+}

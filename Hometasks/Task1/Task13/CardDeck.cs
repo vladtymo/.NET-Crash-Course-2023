8b738df5d1f71c0527e42fd883de6667e3bf4dc5
NikHusachenko <nikgusachenko@gmail.com>
2023-03-20 13:32:39 +0200

    Add task13

---
+++ Hometasks/Task1/Task13/CardDeck.cs
@@ -0,0 +1,93 @@
+﻿namespace Task13
+{
+    public class CardDeck
+    {
+        private Queue<string> cards;
+
+        public CardDeck()
+        {
+            cards = new Queue<string>();
+            string[] suits = { "Hearts", "Diamonds", "Clubs", "Spades" };
+            string[] ranks = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+
+            // Fill the deck with cards
+            foreach (string suit in suits)
+            {
+                foreach (string rank in ranks)
+                {
+                    cards.Enqueue(rank + " of " + suit);
+                }
+            }
+        }
+
+        public void Shuffle()
+        {
+            // Convert the queue to a list
+            List<string> cardList = new List<string>(cards);
+
+            // Shuffle the list using Fisher-Yates shuffle algorithm
+            Random random = new Random();
+            int n = cardList.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = random.Next(n + 1);
+                string temp = cardList[k];
+                cardList[k] = cardList[n];
+                cardList[n] = temp;
+            }
+
+            // Convert the shuffled list back to a queue
+            cards = new Queue<string>(cardList);
+        }
+
+        public string DealOneCard()
+        {
+            if (cards.Count > 0)
+            {
+                return cards.Dequeue();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public List<string> DealCards(int count)
+        {
+            List<string> hand = new List<string>();
+            for (int i = 0; i < count; i++)
+            {
+                string card = DealOneCard();
+                if (card != null)
+                {
+                    hand.Add(card);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return hand;
+        }
+
+        public List<List<string>> DealHands(int handCount, int cardsPerHand)
+        {
+            List<List<string>> hands = new List<List<string>>();
+            for (int i = 0; i < handCount; i++)
+            {
+                List<string> hand = DealCards(cardsPerHand);
+                hands.Add(hand);
+            }
+            return hands;
+        }
+
+        public void PrintDeck()
+        {
+            foreach(string card in cards)
+            {
+                Console.WriteLine(card);
+            }
+        }
+    }
+}
---
+++ Hometasks/Task1/Task13/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace Task13
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Phonebook book = new Phonebook();
+
+            book.Book.Add("+38 093 12 34 567", new Subscriber()
+            {
+                Name = "Vasia",
+                Surname = "Vasichkin",
+            });
+            book.Book.Add("+38 093 23 45 688", new Subscriber()
+            {
+                Name = "Patya",
+                Surname = "Petechkin",
+            });
+            book.Book.Add("+38 093 45 67 890", new Subscriber()
+            {
+                Name = "Maxim",
+                Surname = "Maximov",
+            });
+            book.Book.Add("+38 093 56 34 123", new Subscriber()
+            {
+                Name = "Petr",
+                Surname = "Petrovich",
+            });
+
+            Console.WriteLine(book.Book["+38 093 23 45 688"].ToString());*/
+
+            CardDeck deck = new CardDeck();
+            deck.Shuffle();
+            deck.PrintDeck();
+            
+            List<string> firstHand = deck.DealCards(6);
+            List<string> secondHand = deck.DealCards(6);
+
+            deck.PrintDeck();
+        }
+
+        static List<string> FindWordsWithMaxLength(List<string> words)
+        {
+            words = words.OrderByDescending(word => word.Length)
+                .ToList();
+
+            int maxLength = words[0].Length;
+
+            for (int i = 0; i < words.Count; i++)
+            {
+                if (words[i].Length < maxLength)
+                {
+                    words.Remove(words[i]);
+                }
+            }
+
+            return words;
+        }
+    }
+
+    public class Phonebook
+    {
+        public Dictionary<string, Subscriber> Book { get; }
+
+        public Phonebook()
+        {
+            Book = new Dictionary<string, Subscriber>();
+        }
+    }
+
+    public class Subscriber
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}; Surname: {Surname};";
+        }
+    }
+}

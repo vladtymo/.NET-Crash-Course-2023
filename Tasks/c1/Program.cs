3b3615b58c07462dfab9e146864de2c153cd8a69
makskorney <maks32273@gmail.com>
2023-03-20 14:33:11 +0200

    Add Task 1.

---
+++ Tasks/c1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace c1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Title = "Task 1 ";
+            Console.WriteLine("\n\n\n\t\t\tПочаток введення дати");
+
+            Console.Write("\tВведіть день: ");
+            int day = int.Parse(Console.ReadLine());
+            Console.Write("\tВведіть місяць: ");
+            int mouth = int.Parse(Console.ReadLine());
+            Console.Write("\tВведіть рік: ");
+            int year = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine($"\n\n\t{day}/{mouth}/{year}");
+        }
+    }
+}

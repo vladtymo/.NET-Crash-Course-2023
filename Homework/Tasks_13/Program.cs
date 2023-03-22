1ebe3aaddda9fc589dce0ae825cf5e90a33c170c
Oleg <olegredko45@gmail.com>
2023-03-22 11:44:34 +0200

    add Tasks 13

---
+++ Homework/Tasks_13/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Tasks_13
+{
+	public class Program
+	{
+		public static char[] GetRandomWord()
+		{
+			int randomWordLength = new Random().Next(4, 20);
+			char[] chars = Enumerable.Range(0, randomWordLength)
+				.Select(x => (char)('a' + new Random().Next(26)))
+				.ToArray();
+			return chars;
+		}
+		public static void Task1()
+		{
+			List<string> words = new List<string>();
+			for (int i = 0; i < 10; i++)
+				words.Add(new string(GetRandomWord()));
+
+			Console.WriteLine("Words: ");
+			foreach (string word in words)
+			{
+				Console.WriteLine(word + $" {word.Length} , ");
+			}
+			string longestWord = "";
+			for (int i = 0; i < words.Count; i++)
+			{
+				if (words[i].Length > longestWord.Length)
+				{
+					longestWord = words[i];
+				}
+			}
+			Console.WriteLine("The longest word: " + longestWord);
+		}
+		public class PhoneBook <Tkey,Tvalue>
+		{
+			public PhoneBook() 
+			{
+				PhoneNumbers = new Dictionary<Tkey, Tvalue>();
+			}
+			public Dictionary<Tkey,Tvalue> PhoneNumbers { get; set; }
+		}
+		public static void Task2()
+		{
+
+		}
+		static void Main(string[] args)
+		{
+			Task1();
+			PhoneBook<string,string> phoneBook= new PhoneBook<string,string>();
+			// Add some phone numbers
+			phoneBook.PhoneNumbers.Add("John Smith", "(555) 123-4567");
+			phoneBook.PhoneNumbers.Add("Jane Doe", "(555) 987-6543");
+
+			foreach (KeyValuePair<string, string> entry in phoneBook.PhoneNumbers)
+			{
+				Console.WriteLine("{0}: {1}", entry.Key, entry.Value);
+			}
+
+			Console.ReadLine();
+		}
+	}
+}

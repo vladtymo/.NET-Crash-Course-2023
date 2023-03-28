ca9984d9351eb1c468958fc1d8d84fedb8af8c77
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2023-03-28 20:13:03 +0300

    Entity Framework Core base example.

---
+++ C# Lessons/23_ef_base/Data/LibraryDbContext.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_ef_base.Data
+{
+    internal class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext()
+        {
+            //this.Database.EnsureDeleted();
+            // create database if not exists
+            this.Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            // connection string contains all neccessary info for connect to the db
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Library_DB_Crash;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // data initialization
+            modelBuilder.Entity<Author>().HasData(new[]
+            {
+                new Author()
+                {
+                    Id = 1,
+                    FirstName = "Ivan",
+                    LastName = "Franko",
+                    Country = "Ukraine"
+                },
+                new Author()
+                {
+                    Id = 2,
+                    FirstName = "Taras",
+                    LastName = "Shevchenko",
+                    Country = "Ukraine"
+                },
+                new Author()
+                {
+                    Id = 3,
+                    FirstName = "Mykola",
+                    LastName = "Gogol",
+                    Country = "Ukraine"
+                },
+                new Author()
+                {
+                    Id = 4,
+                    FirstName = "Mark",
+                    LastName = "Twain",
+                    Country = "USA"
+                },
+                new Author()
+                {
+                    Id = 5,
+                    FirstName = "Stephen",
+                    LastName = "King",
+                    Country = "USA"
+                }
+            });
+        }
+
+        // ---------- data collections (tables) ----------
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+    }
+
+    // ---------- entities ----------
+    class Book
+    {
+        // properties (columns)
+        public int Id { get; set; }         // Primary Key: Id/ID/id EntityName+Id
+        public string Title { get; set; }   // NvarChar(MAX) - 2 Gb
+        public int Pages { get; set; }
+        public decimal Price { get; set; }
+        public int AuthorId { get; set; }   // Foreign Key: RelateEntity+PrimaryKeyName
+
+        // navigation properties - reference to an another column
+        public Author Author { get; set; }
+    }
+    // ...
+    // Relationship: One to Many (1...*)
+    // ...
+    class Author
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Country { get; set; }
+
+        // navigation properties
+        public ICollection<Book> Books { get; set; }
+    }
+}
---
+++ C# Lessons/23_ef_base/Program.cs
@@ -0,0 +1,59 @@
+﻿using _23_ef_base.Data;
+
+namespace _23_ef_base
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LibraryDbContext context = new LibraryDbContext();
+
+            // ------------ READ
+            var result = context.Authors.ToList();
+
+            Console.WriteLine("----------- Authors:");
+            foreach (var item in result)
+            {
+                Console.WriteLine($"[{item.Id}] - {item.FirstName} {item.LastName}, {item.Country}");
+            }
+
+            // get by id
+            Console.WriteLine("Enter author id to find:");
+            int id = int.Parse(Console.ReadLine());
+
+            var author = context.Authors.Find(id);
+
+            if (author == null) Console.WriteLine("Author with your id not found!");
+            else
+                Console.WriteLine($"Found author: {author.FirstName} {author.LastName}!");
+
+            // ------------ INSERT
+            Console.WriteLine("------- Add New Book");
+
+            Console.Write("Enter book info (title, price, pages, author id):");
+            Book newBook = new Book()
+            {
+                Title = Console.ReadLine(),
+                Price = decimal.Parse(Console.ReadLine()),
+                Pages = int.Parse(Console.ReadLine()),
+                AuthorId = int.Parse(Console.ReadLine()),
+            };
+            context.Books.Add(newBook);
+
+            // ------------ UPDATE
+            if (author != null)
+                author.FirstName += " de";
+
+            // ------------ DELETE
+            if (author != null)
+                context.Authors.Remove(author);
+
+            context.SaveChanges(); // submit all changes to db
+
+            // ---------------------------- LINQ
+            var myBooks = context.Books.Where(x => x.Price < 1000).OrderByDescending(x => x.Pages).ToList();
+
+            Console.WriteLine("Found books count: " + myBooks.Count);
+        }
+    }
+}

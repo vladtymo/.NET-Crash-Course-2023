867342ca9970cbfc5956e15792868865d8df6233
Karina Zubko <karinazubko2017@gmail.com>
2023-03-30 15:56:38 +0200

    homework for the fifteenth lecture

--- lesson_15/lesson_15.cs
+++ lesson_15/lesson_15.cs
@@ -1,32 +1,43 @@
 using System;
 
-public class Program {
+public class Program{
     public delegate int ArrayVolume(int index);
-
-    public static void Initialization(int[] array, ArrayVolume volume) {
-        for (int i = 0; i < array.Length; i++) {
+    public static void Initialization(int[] array, ArrayVolume volume){
+        for (int i = 0; i < array.Length; i++){
             array[i] = volume(i);
         }
     }
 
-    public static void Main(){
+    public static int[] RandomArray(int size){
         Random randomValue = new Random();
-        //1
-        int[] randomArray = new int[15];
+        int[] randomArray = new int[size];
         Initialization(randomArray, index => randomValue.Next(0, 101));
-        //2 (math.pow(2, index) return double) use (int)
-        int[] powerOfTwoArray = new int[10];
+        return randomArray;
+    }
+
+    public static int[] PowerOfTwoArray(int size){
+        int[] powerOfTwoArray = new int[size];
         Initialization(powerOfTwoArray, index => (int)Math.Pow(2, index));
-        //3
-        int[] stepIncreaseByTreeArray = new int[20];
+        return powerOfTwoArray;
+    }
+
+    public static int[] StepIncreaseByTreeArray(int size){
+        int[] stepIncreaseByTreeArray = new int[size];
         Initialization(stepIncreaseByTreeArray, index => index * 3);
-        //4
-        int[] fibonacciArray = new int[10];
+        return stepIncreaseByTreeArray;
+    }
+
+    public static int[] FibonacciArray(int size){
+        int[] fibonacciArray = new int[size];
         Initialization(fibonacciArray, index => index < 2 ? index : fibonacciArray[index - 1] + fibonacciArray[index - 2]);
-        //output
-        Console.WriteLine("random array:" + string.Join(", ", randomArray));
-        Console.WriteLine("power of two array:" + string.Join(", ", powerOfTwoArray));
-        Console.WriteLine("step increase by tree array:" + string.Join(", ", stepIncreaseByTreeArray));
-        Console.WriteLine("fibonacci array:" + string.Join(", ", fibonacciArray));
+        return fibonacciArray;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("random array:" + string.Join(", ", RandomArray(15)));
+        Console.WriteLine("power of two array:" + string.Join(", ", PowerOfTwoArray(10)));
+        Console.WriteLine("step increase by tree array:" + string.Join(", ", StepIncreaseByTreeArray(20)));
+        Console.WriteLine("fibonacci array:" + string.Join(", ", FibonacciArray(10)));
     }
 }

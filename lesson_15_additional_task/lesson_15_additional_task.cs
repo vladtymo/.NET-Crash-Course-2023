c5a3cbf9e1dcbe839f9001a8eac05a5ad5bf4089
Karina Zubko <karinazubko2017@gmail.com>
2023-03-31 21:22:36 +0200

    additional homework for the fifteenth lecture

--- lesson_15_additional_task/lesson_15_additional_task.cs
+++ lesson_15_additional_task/lesson_15_additional_task.cs
@@ -1,38 +1,44 @@
 using System;
 using System.Collections.Generic;
 
-public class OperationDelegate{
+public class OperationExecutor{
     public delegate double Operation(double x, double y);
     public Operation OperationMethod { get; set; }
-    public OperationDelegate(Operation operation){
+    public OperationExecutor(Operation operation){
         OperationMethod = operation;
     }
-    public double Execute(double x, double y = 0){
+    public double Perform(double x, double y = 0){
         return OperationMethod(x, y);
     }
 }
 
-public class Calculator{
-    private Dictionary<string, OperationDelegate> operations = new Dictionary<string, OperationDelegate>();
-    public Calculator(){
-        RegisterOperation("+", (x, y) => x + y);
-        RegisterOperation("-", (x, y) => x - y);
-        RegisterOperation("*", (x, y) => x * y);
-        RegisterOperation("/", (x, y) => y != 0 ? x / y : double.NaN);
-        RegisterOperation("sin", (x, _) => Math.Sin(x));
-        RegisterOperation("cos", (x, _) => Math.Cos(x));
-        RegisterOperation("tg", (x, _) => Math.Tan(x));
+public class MathCalculator{
+    private Dictionary<string, OperationExecutor> mathOperations = new Dictionary<string, OperationExecutor>();
+    public MathCalculator(){
+        InitializeOperations();
     }
-    public void RegisterOperation(string name, OperationDelegate.Operation operation){
-        operations[name] = new OperationDelegate(operation);
+
+    private void InitializeOperations(){
+        RegisterOperation("add", (x, y) => x + y);
+        RegisterOperation("subtract", (x, y) => x - y);
+        RegisterOperation("multiply", (x, y) => x * y);
+        RegisterOperation("divide", (x, y) => y != 0 ? x / y : double.NaN);
+        RegisterOperation("sine", (x, _) => Math.Sin(x));
+        RegisterOperation("cosine", (x, _) => Math.Cos(x));
+        RegisterOperation("tangent", (x, _) => Math.Tan(x));
     }
-    public double Calculate(string operationName, double x, double y = 0){
-        if (operations.TryGetValue(operationName, out var operation)){
-            return operation.Execute(x, y);
+
+    public void RegisterOperation(string name, OperationExecutor.Operation operation){
+        mathOperations[name] = new OperationExecutor(operation);
+    }
+
+    public double Evaluate(string operationName, double x, double y = 0){
+        if (mathOperations.TryGetValue(operationName, out var operation)){
+            return operation.Perform(x, y);
         }
         else{
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"error '{operationName}'");
+            Console.WriteLine($"Error: '{operationName}'");
             Console.ResetColor();
             return double.NaN;
         }
@@ -41,14 +47,13 @@ public class Calculator{
 
 public class Program{
     public static void Main(){
-        Calculator calculator = new Calculator();
-        double x = 3.0;
-        double y = 2.0;
-        string input = "+ 3 2 - * / sin cos tg error";
+        MathCalculator mathCalculator = new MathCalculator();
+        double x = 9.0;
+        double y = 1.0;
+        string input = "add subtract multiply divide sine cosine tangent error";
         string[] tokens = input.Split(' ');
-        for (int i = 0; i < tokens.Length; i++){
-            string operationName = tokens[i];
-            double result = calculator.Calculate(operationName, x, y);
+        foreach (string operationName in tokens){
+            double result = mathCalculator.Evaluate(operationName, x, y);
             if (!double.IsNaN(result)){
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine($"{operationName}({x}, {y}): {result}");

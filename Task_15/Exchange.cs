4a99fe7b59989bddbb7d33664523a8124c85cd07
Stepan-2007 <manua.stepan.2007@gmail.com>
2023-04-04 11:40:11 +0300

    Task_15

---
+++ Task_15/Exchange.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_15
+{
+    internal class Exchange
+    {
+        public Exchange()
+        {
+            ExchangeRate = new Random().Next(1, 100);
+        }
+        public int ExchangeRate { get; set; }
+
+        public event Action<int> UpdateDay;
+
+        public void Update()
+        {
+            UpdateDay?.Invoke(ExchangeRate);
+        }
+    }
+}
---
+++ Task_15/Human.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_15
+{
+    internal class Human
+    {
+        public Human(string name)
+        {
+            Name = name;
+            Money = new Random().Next(1, 100);
+            CompanyStocks = 0;
+            PurchaseRate = 0;
+        }
+
+        public string Name { get; set; }
+        public int Money { get; set; }
+        public int CompanyStocks { get; set; }
+        public int PurchaseRate { get; set; }
+
+        public void Trading(int exchangeRate)
+        {
+            if (exchangeRate > this.PurchaseRate)
+            {
+                this.Money += this.CompanyStocks * exchangeRate;
+                this.CompanyStocks = this.Money / exchangeRate;
+                this.Money -= this.CompanyStocks * exchangeRate;
+                this.PurchaseRate = exchangeRate;
+                Console.WriteLine("Good day for trading");
+            }
+            else
+            {
+                Console.WriteLine("Bad day");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Name {Name}, Money {Money}, CompanyStocks - {CompanyStocks}, PurchaseRate - {PurchaseRate}";
+        }
+    }
+}
